39087212aa9adf6c21092faddeac434e5a8ffabe
nuttapon <nuttapon@code-app.com>
2014-01-23 18:23:46 +0700

    git rm delete

--- db/migrate/20140122060102_add_status_to_friend.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToFriend < ActiveRecord::Migration
-  def change
-    add_column :friends, :status, :integer, default: 1
-  end
-end
--- db/migrate/20140122061535_add_public_member_to_member.rb
+++
@@ -1,5 +0,0 @@
-class AddPublicMemberToMember < ActiveRecord::Migration
-  def change
-    add_column :members, :public_member, :boolean, default: true
-  end
-end
