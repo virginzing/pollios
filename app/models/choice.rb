36750336a3269b5a0050544cccadd78f4d5f5c87
nuttapon <nuttapon@code-app.com>
2014-02-04 10:38:19 +0700

    add default scope choice

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,6 +2,8 @@ class Choice < ActiveRecord::Base
   belongs_to :poll
   validates :answer, presence: true
 
+  default_scope { order("id asc") }
+  
   def self.create_choices(poll_id ,list_choice)
     list_choice.collect! { |answer| create(poll_id: poll_id, answer: answer) }
   end
