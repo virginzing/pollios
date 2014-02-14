e294b96fa3d033d61cef8bbabac558e3c9fbaa4a
nuttapon <nuttapon@code-app.com>
2014-02-15 03:10:08 +0700

    edit poll series to public

--- app/models/poll_series.rb
+++ app/models/poll_series.rb
@@ -13,7 +13,7 @@ class PollSeries < ActiveRecord::Base
   def set_poll_series
     self.number_of_poll = polls.count
     self.save
-    polls.collect{ |poll| poll.update(expire_date: self.expire_date, series: true, choice_count: poll.choices.count ) }
+    polls.collect{ |poll| poll.update(expire_date: self.expire_date, series: true, choice_count: poll.choices.count, public: true ) }
     PollMember.create!(member_id: self.member_id, poll_id: polls.last.id)
   end
 end
