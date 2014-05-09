43c1c995115856e44e45f4678feae4aeb74f3bf4
nuttapon <nuttapon@code-app.com>
2014-05-09 15:31:13 +0700

    ignore DS store

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -171,8 +171,8 @@ end
 # Poll.joins(:poll_members).includes(:poll_series, :member).where("poll_members.poll_id < ? AND (poll_members.member_id IN (?) OR public = ?)", 2000, [1,2,3], true).order("poll_members.created_at desc")
 # curl -H "Content-Type: application/json" -d '{
 #     "member_id": 1,
-#     "choice_id": 792
-# }' -X POST http://localhost:3000/poll/287/vote.json -i
+#     "choice_id": 789
+# }' -X POST http://localhost:3000/poll/286/vote.json -i
 
 
 # Tag.find_by_name("codeapp").polls.
