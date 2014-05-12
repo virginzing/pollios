c745296070e6eeeca859f2e64cf59b9f019a5b32
nuttapon <nuttapon@code-app.com>
2014-05-12 12:02:46 +0700

    edit public timeline

--- app/services/public_timelinable.rb
+++ app/services/public_timelinable.rb
@@ -21,14 +21,10 @@ class PublicTimelinable
   def poll_public
     @hidden_poll_ids = my_hidden
     if @hidden_poll_ids.empty?
-      # query = Poll.unscoped.joins(:choices).select("polls.*, max(choices.vote) as vote_max, choices.answer as choice_answer").
-      #               group("polls.id, choices.poll_id, choices.answer").order("vote_max desc, created_at desc").
-      #               joins(:poll_members).includes(:member, :poll_series, :campaign).
-      #               where("poll_members.public = ? AND share_poll_of_id = 0", true)
       query = Poll.joins(:poll_members).includes(:choices, :member, :poll_series, :campaign).
                    where("poll_members.public = ? AND share_poll_of_id = 0 AND poll_members.in_group = ? AND poll_members.expire_date > ?", true, false, Time.now)
     else
-    query = Poll.joins(:poll_members).includes(:choices, :member, :poll_series, :campaign).
+      query = Poll.joins(:poll_members).includes(:choices, :member, :poll_series, :campaign).
                  where("poll_members.public = ? AND poll_members.share_poll_of_id = 0 AND poll_members.poll_id NOT IN (?) AND poll_members.in_group = ? AND poll_members.expire_date > ?", true, @hidden_poll_ids, false, Time.now)
     end
     filter_type(query, type)
