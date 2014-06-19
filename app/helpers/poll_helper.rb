283842b50d41a4939fdbc4a8e2524f9892f736c0
nuttapon <nuttapon@code-app.com>
2014-06-19 21:45:06 +0700

    add poll stats

--- app/helpers/poll_helper.rb
+++ app/helpers/poll_helper.rb
@@ -5,11 +5,22 @@ module PollHelper
     number_to_percentage((vote*100)/vote_all.to_f, precision: 0)
   end
 
-  def polls_30_days_ago
-    (1.months.ago.to_date..Date.today).map do |date|
+  def polls_30_days_ago_chart(start = 1.month.ago)
+    polls_by_day = Poll.total_grouped_by_date(start)
+    
+    polls_friend_by_day = Poll.where("public = ? AND in_group_ids = ?", false, '0').total_grouped_by_date(start)
+
+    polls_public_by_day = Poll.where("public = 't'").total_grouped_by_date(start)
+
+    polls_group_by_day = Poll.where("public = ? AND in_group_ids != ?", false, '0').total_grouped_by_date(start)
+
+    (start.to_date..Date.today).map do |date|
       {
         created_at: date,
-        count: Poll.where("date(created_at) = ?", date).count
+        count: polls_by_day[date] || 0,
+        poll_of_friend: polls_friend_by_day[date] || 0,
+        poll_of_public: polls_public_by_day[date] || 0,
+        poll_of_group: polls_group_by_day[date] || 0
       }
     end
   end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -564,6 +564,17 @@ class Poll < ActiveRecord::Base
    }
   end
 
+  def self.total_grouped_by_date(start)
+    polls = where(created_at: start.beginning_of_day..Time.zone.now)
+    polls = polls.group("date(created_at)")
+    polls = polls.select("date(created_at) as created_at, count(*) as total_poll")
+
+    polls.each_with_object({}) do |poll, hsh|
+      hsh[poll.created_at.to_date] = poll.total_poll
+    end
+  end
+
+
 
 end
 
