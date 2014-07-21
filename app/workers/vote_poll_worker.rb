83f45b0c675d369d20eb7f9bfe767a6277c785b0
nuttapon <nuttapon@code-app.com>
2014-07-21 22:05:47 +0700

    fix bug

--- app/workers/vote_poll_worker.rb
+++ app/workers/vote_poll_worker.rb
@@ -4,7 +4,7 @@ class VotePollWorker
 
   def perform(member, poll, custom_data = {})
     anonymous = custom_data[:anonymous]
-    @poll = poll
+
     member_id = member.id
     
     @apn_poll = Apn::VotePoll.new(member, poll)
@@ -16,6 +16,8 @@ class VotePollWorker
     # device_ids = find_recipient.collect {|u| u.apn_devices.collect(&:id)}.flatten
 
     find_recipient_notify ||= Member.where(id: recipient_ids - [member_id])
+
+    poll_within_group ||= Group.joins(:poll_groups).where("poll_id = #{poll.id}").uniq
 
     @custom_properties = { 
       poll_id: poll.id
@@ -56,22 +58,28 @@ class VotePollWorker
         poll: PollSerializer.new(poll).as_json()
       }
 
-      group = check_in_group(member)
-
-      if group.present? 
-        hash_custom.merge!( { group: group.as_json() })
+      check_group = check_in_group(member, poll_within_group)
+      # puts "group detail => #{check_group}, group present => #{check_group.present?}"
+      if check_group.present? 
+        # puts "merge group detail"
+        new_hash = hash_custom.merge!( Hash["group" => check_group.as_json()] )
+      else
+        new_hash = hash_custom
       end
 
-      NotifyLog.create!(sender_id: member_id, recipient_id: member.id, message: @apn_poll.custom_message, custom_properties: @custom_properties.merge!(hash_custom))
+      # puts "hash_custom => #{new_hash}"
+
+      NotifyLog.create!(sender_id: member_id, recipient_id: member.id, message: @apn_poll.custom_message, custom_properties: @custom_properties.merge!(new_hash))
     end
 
     Apn::App.first.send_notifications
   end
 
-  def check_in_group(member)
+  def check_in_group(member, poll_with_group)
     group_of_receiver = member.cached_get_group_active
-    poll_in_group = Group.joins(:poll_groups).where("poll_id = #{@poll.id}").uniq
-    group = (group_of_receiver && poll_in_group).first
+    group = (poll_with_group && group_of_receiver).first
+    # puts "member => #{member.id}, group => #{group.as_json}"
+    group
   end
 
 end
