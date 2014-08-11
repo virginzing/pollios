714050251415af13856828cffa6f1780a7cf0382
nuttapon <nuttapon@code-app.com>
2014-08-11 20:31:32 +0700

    fix bug

--- app/services/recommendation.rb
+++ app/services/recommendation.rb
@@ -40,7 +40,7 @@ class Recommendation
 
   def find_mutual_friend
     connection = ActiveRecord::Base.connection
-    query = "SELECT r1.follower_id AS first_user, r2.follower_id AS second_user, COUNT(r1.followed_id) as mutual_friend_count FROM friends r1 INNER JOIN friends r2 ON r1.followed_id = r2.followed_id AND r1.follower_id != r2.follower_id WHERE r1.status = 1 AND r2.status = 1 AND r1.follower_id = #{@member.id} AND r2.follower_id = #{@member.id} GROUP BY r1.follower_id, r2.follower_id"
+    query = "SELECT r1.follower_id AS first_user, r2.follower_id AS second_user, COUNT(r1.followed_id) as mutual_friend_count FROM friends r1 INNER JOIN friends r2 ON r1.followed_id = r2.followed_id AND r1.follower_id != r2.follower_id WHERE r1.status = 1 AND r2.status = 1 AND r1.follower_id = #{@member.id} GROUP BY r1.follower_id, r2.follower_id"
     list_recomment = ActiveRecord::Base.connection.execute(query)
   end
 
