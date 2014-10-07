d1c1dae98c7204a28c67af2b68427c818c3a6ff4
nuttapon <nuttapon@code-app.com>
2014-10-07 21:33:27 +0700

    add key comment for delete log

--- app/models/delete_poll.rb
+++ app/models/delete_poll.rb
@@ -6,12 +6,14 @@ class DeletePoll
   field :creator, type: Hash
   field :poll, type: Hash
   field :voter, type: Array
+  field :comment, type: Array
   field :delete_at, type: Time, default: -> { Time.now }
+
 
   def self.create_log(poll)
     if (poll.vote_all > 0 || poll.view_all > 0)
       voter = ActiveModel::ArraySerializer.new(poll.history_votes, each_serializer: VoterFordeleteSerializer).as_json()
-      DeletePoll.create!(creator: poll.member.as_json(), poll: PollForDeleteSerializer.new(poll).as_json(), voter: voter)
+      DeletePoll.create!(creator: poll.member.as_json(), poll: PollForDeleteSerializer.new(poll).as_json(), voter: voter, comment: poll.comments.as_json())
     end
   end
 
