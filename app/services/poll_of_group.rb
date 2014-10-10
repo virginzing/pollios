d22e18270e0b8e2e79bdb59b889ee5f3eea2a972
nuttapon <nuttapon@code-app.com>
2014-10-10 10:34:30 +0700

    pull questionnaire voted off

--- app/services/poll_of_group.rb
+++ app/services/poll_of_group.rb
@@ -23,23 +23,36 @@ class PollOfGroup
   end
 
   def get_poll_available_of_group
-    @only_poll_available = true
-    @poll_of_group ||= poll_of_group
+    @poll_of_group ||= api_poll_of_group
   end
 
   def get_poll_of_group_company
     @poll_of_group_company ||= poll_of_group_company
   end
 
+  def my_vote_questionnaire_ids
+    Member.voted_polls.select{|e| e["poll_series_id"] != 0 }.collect{|e| e["poll_id"] }
+  end
+
   private
 
   def poll_of_group
     poll_group_query = "poll_groups.group_id = #{@group.id}"
-    @query = Poll.available.order("updated_at DESC, created_at DESC").joins(:groups).includes(:history_votes, :member)
-                .select("polls.*, poll_groups.share_poll_of_id as share_poll, poll_groups.group_id as group_of_id")
-                .where("#{poll_group_query}").uniq
-    @query = @query.available if @only_poll_available
+    @query = Poll.order("updated_at DESC, created_at DESC").joins(:groups).includes(:history_votes, :member)
+                  .select("polls.*, poll_groups.share_poll_of_id as share_poll, poll_groups.group_id as group_of_id")
+                  .where("#{poll_group_query}").uniq
     @query
+  end
+
+  def api_poll_of_group
+    poll_group_query = "poll_groups.group_id = #{@group.id}"
+    query = Poll.available.order("updated_at DESC, created_at DESC").joins(:groups).includes(:history_votes, :member)
+                  .select("polls.*, poll_groups.share_poll_of_id as share_poll, poll_groups.group_id as group_of_id")
+                  .where("#{poll_group_query}").uniq
+
+    query = query.where("polls.id NOT IN (?)", my_vote_questionnaire_ids) if my_vote_questionnaire_ids.count > 0
+
+    query
   end
 
   def poll_of_group_company
@@ -54,5 +67,5 @@ class PollOfGroup
   def poll_unexpire
     "polls.expire_date > '#{Time.now}'"
   end
-  
+
 end
