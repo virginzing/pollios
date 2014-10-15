63b0549f8966bc9c60d80048aa77b7e628b45f3b
nuttapon <nuttapon@code-app.com>
2014-10-15 12:57:05 +0700

    fix bug

--- app/services/which_poll.rb
+++ app/services/which_poll.rb
@@ -9,7 +9,7 @@ class WhichPoll
   def self.to_hash(value = nil)
     case value
       when 0 then Hash["in" => "Public"]
-      when 1 then Hash["in" => "Friend & Following"]
+      when 1 then Hash["in" => "Friends & Following"]
       when 2 then Hash["in" => "Group"]
     end
   end
