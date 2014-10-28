a31af185e11531c0910fe2dbaf959a283b23b28d
nuttapon <nuttapon@code-app.com>
2014-10-28 17:46:42 +0700

    remove pollios exclusive

--- app/models/member.rb
+++ app/models/member.rb
@@ -126,8 +126,8 @@ class Member < ActiveRecord::Base
   scope :citizen,   -> { where(member_type: 0) }
   scope :celebrity, -> { where(member_type: 1) }
 
-  validates :email, presence: true, :uniqueness => { :case_sensitive => false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :username , :uniqueness => { :case_sensitive => false}, format: { with: /\A[a-zA-Z0-9_.]+\z/i, message: "only allows letters" }, :allow_blank => true , on: :update
+  validates :email, presence: true, :uniqueness => { :case_sensitive => false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, :allow_blank => true 
+  validates :username , :uniqueness => { :case_sensitive => false }, format: { with: /\A[a-zA-Z0-9_.]+\z/i, message: "only allows letters" }, :allow_blank => true , on: :update
 
   LIMIT_POLL = 10
   self.per_page = 20
