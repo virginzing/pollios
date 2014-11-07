1f14a0a2991f94f34e0d64410d610a3168098fca
nuttapon <nuttapon@code-app.com>
2014-11-07 14:56:17 +0700

    test apn

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -5,3 +5,13 @@ configatron.apn.host = 'gateway.sandbox.push.apple.com'
 configatron.apn.feedback.passphrase = '1150'
 configatron.apn.feedback.port = 2196
 configatron.apn.feedback.host = 'feedback.sandbox.push.apple.com'
+
+
+# apn = Apn::Notification.new
+#   apn.device_id = 60
+#   apn.badge = 1
+#   apn.alert = "11"
+#   apn.sound = true
+# apn.save
+
+# Apn::App.first.send_notifications
