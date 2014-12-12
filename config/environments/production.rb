1f52ddccba52f9deaae0e313eec7d3d4d85374b9
Nuttapon <nuttapon@code-app.com>
2014-12-12 21:13:44 +0700

    rollback production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,16 +81,16 @@ Pollios::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  # config.cache_store = :dalli_store,
-  #                   (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-  #                   {:username => ENV["MEMCACHIER_USERNAME"],
-  #                    :password => ENV["MEMCACHIER_PASSWORD"],
-  #                    :failover => true,
-  #                    :socket_timeout => 1.5,
-  #                    :socket_failure_delay => 0.2,
-  #                    :compress => true
-  #                   }
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :compress => true
+                    }
 
-  config.cache_store = :dalli_store, nil,  { compress: true }
+  # config.cache_store = :dalli_store, nil,  { compress: true }
 
 end
