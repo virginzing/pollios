ca2b3c7a9e39e859ef65322b99951d74de1cb6bd
nuttapon <nuttapon@code-app.com>
2015-01-26 17:14:04 +0700

    add breadcrum v2

--- config/breadcrumbs/company.rb
+++ config/breadcrumbs/company.rb
@@ -6,3 +6,12 @@ crumb :company_new_poll do
   link "New Poll"
   parent :company_polls
 end
+
+crumb :company_poll_detail do |poll|
+  link poll.title, company_polls_detail_path(poll)
+  parent :company_polls
+end
+
+crumb :company_questionnaires do
+  link "All Questionnaires", company_questionnaires_path
+end
