e29da6464730673f13fd8df910f1028b129ec704
nuttapon <nuttapon@code-app.com>
2015-03-26 15:04:19 +0700

    add edit form of special qrcode

--- app/controllers/special_qrcodes_controller.rb
+++ app/controllers/special_qrcodes_controller.rb
@@ -1,6 +1,7 @@
 class SpecialQrcodesController < ApplicationController
   layout 'admin'
 
+  before_filter :set_special_qrcode, only: [:edit, :show, :update]
 
   def new
     @special_qrcode = SpecialQrcode.new
@@ -24,9 +25,22 @@ class SpecialQrcodesController < ApplicationController
   end
 
   def show
-    @special_qrcode = SpecialQrcode.find(params[:id])
     # text_special_qrcode = Hash["code" => @special_qrcode.code, "type" => @special_qrcode.info["type"] ]
     @qr = RQRCode::QRCode.new(GenerateSpecialQrcodeLink.new(@special_qrcode.code, @special_qrcode.info["type"]).get_redirect_link, :size => 8, :level => :h ).to_img.resize(200, 200).to_data_url
+  end
+
+  def update
+    if @special_qrcode.update!(special_qrcode_params)
+      flash[:success] = "Successfully updated"
+      redirect_to admin_special_qrcode_path
+    else
+      flash[:error] = "Update fail"
+      redirect_to edit_special_qrcode_path(@special_qrcode)
+    end
+  end
+
+  def edit
+    
   end
 
   def destroy
@@ -35,6 +49,10 @@ class SpecialQrcodesController < ApplicationController
 
 
   private
+
+  def set_special_qrcode
+    @special_qrcode = SpecialQrcode.find(params[:id])
+  end
 
   def special_qrcode_params
     params.require(:special_qrcode).permit(:code, :member_id, :type)
