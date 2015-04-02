025f060e08c781208ec3bd1f42c4cf2e67d5dec0
nuttapon <nuttapon@code-app.com>
2015-04-02 11:34:13 +0700

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -224,17 +224,17 @@ class ApplicationController < ActionController::Base
       init_list_poll = Member::ListPoll.new(Member.current_member)
       init_list_group = Member::ListGroup.new(Member.current_member)
 
-      Member.list_friend_active ||= init_list_friend.active
-      Member.list_friend_block ||= init_list_friend.block
-      Member.list_friend_request ||= init_list_friend.friend_request
-      Member.list_your_request ||= init_list_friend.your_request
-      Member.list_friend_following ||= init_list_friend.following
+      Member.list_friend_active = init_list_friend.active
+      Member.list_friend_block = init_list_friend.block
+      Member.list_friend_request = init_list_friend.friend_request
+      Member.list_your_request = init_list_friend.your_request
+      Member.list_friend_following = init_list_friend.following
 
-      Member.list_group_active ||= init_list_group.active
-      Member.reported_polls ||= init_list_poll.reports
-      Member.viewed_polls   ||= init_list_poll.history_viewed
-      Member.voted_polls    ||= init_list_poll.voted_all
-      Member.watched_polls  ||= init_list_poll.watches
+      Member.list_group_active = init_list_group.active
+      Member.reported_polls = init_list_poll.reports
+      Member.viewed_polls   = init_list_poll.history_viewed
+      Member.voted_polls    = init_list_poll.voted_all
+      Member.watched_polls  = init_list_poll.watches
       # p "=== End ==="
     end
   end
--- spec/apis/poll_created_spec.rb
+++ spec/apis/poll_created_spec.rb
@@ -5,6 +5,9 @@ describe "GET /friend/polls", type: :api do
   let!(:member) { create(:member, fullname: "Nutkub", email: "nutkub@gmail.com") }
   let!(:friend) { create(:member, fullname: "Ning", email: "ning@gmail.com") }
 
+  let!(:friend1) { create(:friend, follower: member, followed: friend, active: true, status: 1) }
+  let!(:friend2) { create(:friend, follower: friend, followed: member, active: true, status: 1) }
+
   before do
     3.times do
       Poll.create_poll(FactoryGirl.attributes_for(:create_poll).merge(member_id: member.id), member)
@@ -22,11 +25,6 @@ describe "GET /friend/polls", type: :api do
   end
 
   context 'when access to friend created poll' do
-    before do
-      Friend.add_friend( {member_id: member.id, friend_id: friend.id})
-      Friend.accept_or_deny_freind({member_id: friend.id, friend_id: member.id}, true)
-    end
-
     it "retrun my friend's created" do
       get 'friend/polls.json', { member_id: member.id, friend_id: friend.id, api_version: 6}, { "Accept" => "application/json" }
       expect(last_response.status).to eq(200)
