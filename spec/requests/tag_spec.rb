1e47e29df5484c8c753615175ba14fa497b2f634
nuttapon <nuttapon@code-app.com>
2015-04-07 13:48:57 +0700

    add test hashtag popular

---
+++ spec/requests/tag_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Tag" do
+  let!(:member) { create(:member) }
+
+  # create_poll factory girl have 2 tags by default
+
+  before do
+    Poll.create_poll(FactoryGirl.attributes_for(:create_poll).merge(member_id: member.id), member)
+  end
+
+  describe "GET /hashtag_popular" do
+    before do
+      get "/hashtag_popular.json", { member_id: member.id }, { "Accept" => "application/json" }
+    end
+
+    it "success" do
+      expect(json["response_status"]).to eq("OK")
+      expect(json["tag_lists"].size).to eq(2)    
+    end
+
+  end 
+end
