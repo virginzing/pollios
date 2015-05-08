7d5c707ce424011648df456c21d93d40489b29a5
nuttapon <nuttapon@code-app.com>
2015-05-08 14:59:56 +0700

    add group detail

---
+++ app/decorators/group_decorator.rb
@@ -0,0 +1,12 @@
+class GroupDecorator < ApplicationDecorator
+  include Draper::LazyHelpers
+
+  delegate_all
+
+
+  def show_cover_group
+    image_tag( object.cover.url(:thumbnail_small), data: { toggle: "tooltip", placement: "top", :'original-title' => object.name }, alt: "user", class: 'img img-circle img-responsive center-block' )    
+  end
+
+
+end
