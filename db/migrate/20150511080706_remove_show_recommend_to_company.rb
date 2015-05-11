d9ee446788f7f0026245bd01962e21e767971bab
nuttapon <nuttapon@code-app.com>
2015-05-11 15:12:07 +0700

    fix migration tion

--- db/migrate/20150511080706_remove_show_recommend_to_company.rb
+++ db/migrate/20150511080706_remove_show_recommend_to_company.rb
@@ -1,5 +1,6 @@
 class RemoveShowRecommendToCompany < ActiveRecord::Migration
   def change
+    add_column :companies, :show_recommend, :boolean, default: false
     remove_column :companies, :show_recommend
   end
 end
