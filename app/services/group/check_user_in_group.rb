dfbd3c28fdb52ba082d6f74ed758438042998a35
nuttapon <nuttapon@code-app.com>
2015-05-22 11:58:36 +0700

    fix bug

---
+++ app/services/group/check_user_in_group.rb
@@ -0,0 +1,19 @@
+class Group::CheckUserInGroup
+  def initialize(member, group)
+    @member = member
+    @group = group
+  end
+
+  def member_list_group
+    @member_list_group ||= Member::ListGroup.new(@member).active.map(&:id)
+  end
+
+  def exists?
+    member_list_group.include?(@group.id) ? true : false
+  end
+
+  def raise_error_not_in_group
+    fail ExceptionHandler::NotAcceptable, "You're not in group." unless exists?
+  end
+
+end
