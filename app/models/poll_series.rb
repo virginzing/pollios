cae80f17e34d21bc38c64c39a04b734df763f783
nuttapon <nuttapon@code-app.com>
2015-06-03 17:27:14 +0700

    fix bug delete poll series

--- app/models/poll_series.rb
+++ app/models/poll_series.rb
@@ -7,7 +7,7 @@ class PollSeries < ActiveRecord::Base
   belongs_to :member
   belongs_to :campaign
 
-  has_many :polls, -> { unscope(:order).order('polls.order_poll asc') }
+  has_many :polls, -> { unscope(:order).order('polls.order_poll asc') }, dependent: :destroy
 
   has_many :suggests
 
