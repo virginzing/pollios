33686917c49893cb7d3a17fd1704071af2d8844f
nuttapon <nuttapon@code-app.com>
2015-06-26 21:18:13 +0700

    fix screen cover of group detail

--- app/decorators/group_decorator.rb
+++ app/decorators/group_decorator.rb
@@ -14,7 +14,7 @@ class GroupDecorator < ApplicationDecorator
 
   def show_cover_group_in_company
     if object.cover_preset == "0"
-      image_tag( object.cover.url(:cover), data: { toggle: "tooltip", placement: "top", :'original-title' => object.name }, alt: "user", class: 'img img-responsive center-block' )    
+      image_tag( object.cover.url(:cover), data: { toggle: "tooltip", placement: "top", :'original-title' => object.name }, alt: "user", class: 'img img-responsive center-block', size: "320x130" )    
     else
       image_tag( Group::CoverPresetSelector.new(object).show_cover_preset, class: 'img img-responsive center-block', size: '320x130')
     end
