73a30250b44797e819d5a1c7444888e3e5f1ea07
nuttapon <nuttapon@code-app.com>
2015-07-08 08:40:11 +0700

    fix popular poll in homepage

--- app/services/homepage/popular_poll.rb
+++ app/services/homepage/popular_poll.rb
@@ -2,7 +2,7 @@ module PopularPoll
   module_function
   
   def in_total
-    Poll.public_poll.active_poll.having_status_poll(:gray, :white).limit(3).where("vote_all != 0 AND series = 'f'").order("vote_all desc")
+    Poll.public_poll.unexpire.having_status_poll(:gray, :white).where(draft: false, deleted_at: nil).limit(3).where("vote_all != 0 AND series = 'f'").order("vote_all desc")
   end
   
 end
