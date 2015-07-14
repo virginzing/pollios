5d031ad1b190183e499c09e71259f0bf61fdc9b9
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-14 17:13:28 +0700

    fix comment mention worker

--- app/services/apn/comment_mention.rb
+++ app/services/apn/comment_mention.rb
@@ -3,17 +3,24 @@ class Apn::CommentMention
   include ActionView::Helpers::TextHelper
   include NotificationsHelper
 
-  def initialize(mentioner, poll)
+  def initialize(mentioner, poll, mentionable_list)
     @mentioner = mentioner
     @poll = poll
+    @mentionable_list = mentionable_list
   end
 
   def mentioner_name
     @mentioner.fullname
   end
 
+  def receive_notification
+    list_members = Member.where(id: @mentionable_list)
+    getting_notification(list_members, "watch_poll")
+  end
+
   def custom_message
     message = "#{mentioner_name} mentioned you in a comment"
     truncate_message(message)
   end
+
 end
