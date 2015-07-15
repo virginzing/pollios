5bec494afe82d0df080dc1fecf9b9187da0905ad
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-15 17:15:47 +0700

    trigger can remove

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -1,12 +1,16 @@
 class TriggersController < ApplicationController
   layout 'admin'
 
+  before_action :find_trigger, only: [:destroy]
+
+  skip_before_action :verify_authenticity_token
+
   def index
     @list_triggers = Trigger.all
   end
 
   def new
-    
+
   end
 
   def create
@@ -14,8 +18,21 @@ class TriggersController < ApplicationController
 
     if build_trigger
       flash[:success] = "Create success"
-      redirect_to triggers_path  
+      redirect_to triggers_path
     end
-    
+
+  end
+
+  def destroy
+    @trigger.destroy
+    flash[:success] = "Successfully destroy..."
+    redirect_to triggers_path
+  end
+
+
+  private
+
+  def find_trigger
+    @trigger = Trigger.find(params[:id])
   end
 end
