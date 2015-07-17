230a364a9e7bf10dc7f04f3be736f58411f3e46c
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-17 13:58:36 +0700

    add validates presence and uniqness to PolliosApp

--- app/models/pollios_app.rb
+++ app/models/pollios_app.rb
@@ -3,9 +3,13 @@ class PolliosApp < ActiveRecord::Base
   extend Enumerize
   extend ActiveModel::Naming
 
+  validates :name, :app_id, presence: true
+  validates :app_id, uniqueness: true
+
   enumerize :platform, :in => { unknown: 0, ios: 1, andriod: 2 }, predicates: true, default: :unknown
 
   scope :un_expired, -> { where("expired_at > ? OR expired_at IS NULL", Date.current) }
+  scope :usage_members, -> (app_id) { ApiToken.where(app_id: app_id) }
 
   def self.list_app_ids
     un_expired.map(&:app_id)
