37c3b2506ce4d375134281a1eaa3409bbde486a3
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-23 00:16:16 +0700

    refactoring create poll v1

--- app/services/create_poll_service.rb
+++ app/services/create_poll_service.rb
@@ -22,6 +22,16 @@ class CreatePollService
     params[:choices]
   end
 
+  def init_list_choices_for_new_poll
+    init_list_choices = []
+    list_choices.each do |choice|
+      init_list_choices << {
+        answer: choice
+      }
+    end
+    init_list_choices
+  end
+
   def list_choices_checking
     return unless params[:choices].class == Array
     return unless params[:choices].size > 1
@@ -32,26 +42,40 @@ class CreatePollService
     list_choices.size
   end
 
-  def default_type_poll
+  def set_type_poll
     Poll.type_poll.default_value
   end
 
+  def is_citizen_and_having_point?
+    @creator.citizen? && @creator.point > 0
+  end
+
+  def split_tags_form_title
+    split_tags = []
+    params[:title].gsub(/\B#([[:word:]]+)/) { split_tags << $1 }
+    return split_tags
+  end
+
   def poll_attributes
-    {
-      member_id: @creator.id,
-      title: params[:title],
-      expire_date: convert_expire_date,
-      public: poll_public_status,
-      in_group: in_group_status,
-      in_group_ids: in_group_ids,
-      series: false,
-      poll_series_id: 0,
-      choice_count: calculate_choices,
-      campaign_id: 0,
-      recurring_id: 0,
-      qrcode_key: generate_qrcode_key,
-      type_poll: params[:type_poll] || default_type_poll
-    }
+    params[:member_id] = @creator.id
+    params[:expire_date] = convert_expire_date
+    params[:public] = poll_public_status
+    params[:in_group] = in_group_status
+    params[:in_group_ids] = in_group_ids
+    params[:series] = false
+    params[:poll_series_id] = 0
+    params[:choice_count] = calculate_choices
+    params[:campaign_id] = 0
+    params[:recurring_id] = 0
+    params[:qrcode_key] = generate_qrcode_key
+    params[:member_type] = @creator.member_type_text
+    params[:show_result] = true
+    params[:require_info] = false
+    params[:qr_only] = false
+    params[:priority] = 0
+    params[:thumbnail_type] = params[:thumbnail_type] || 0
+    params[:choices_attributes] = init_list_choices_for_new_poll
+    params.except(:choices, :is_public, :group_id, :photo_poll, :original_images)
   end
 
   def new_poll
@@ -59,28 +83,121 @@ class CreatePollService
   end
 
   def create!
-    if new_poll.save
-      if in_public?
-
+    Poll.transaction do
+      validate_that_can_create!
+      if new_poll.save
+        update_photo_poll if params[:photo_poll].present?
+        additional_poll_attachments if params[:original_images].present?
+        decrease_point! if is_citizen_and_having_point? && in_public?
+        creatable_tagging!
+        watching_poll!
+        add_poll_to_feed!
+        add_poll_to_group! if in_group?
+        add_poll_to_company! if @creator.company?
+        tracking_activtiy_feed! if in_group?
+        MemberActiveRecord.record_member_active(@creator)
+        PollStats.create_poll_stats(@poll)
+        Activity.create_activity_poll(@creator, @poll, 'Create')
+        @poll
       else
-        if in_group?
+        raise ExceptionHandler::UnprocessableEntity, @poll.errors.full_messages.join(", ")
+      end
+    end
+  end
 
-        else
-
+  def validate_that_can_create!
+    if in_public?
+      raise ExceptionHandler::UnprocessableEntity, ExceptionHandler::Message::Poll::POINT_ZERO unless is_citizen_and_having_point?
+    else
+      if in_group? && !@creator.company?
+        unless available_post_to_group_ids.size > 0
+          group_names = Group.where(id: post_to_group_ids)
+          alert_message = "You're no longer in #{group_names.map(&:name).join(', ')}"
+          raise ExceptionHandler::CustomError, { response_status: "ERROR", alert_message: alert_message }
         end
       end
-    else
-      raise ExceptionHandler::UnprocessableEntity, @poll.errors.full_messages.join(', ')
     end
+  end
 
+  def alert_message
+    if (available_post_to_group_ids.size != post_to_group_ids.size) && in_group?
+      group_names = Group.where(id: (post_to_group_ids - available_post_to_group_ids)).map(&:name).join(', ')
+      alert_message = "This poll don't show in #{group_names} because you're no longer these group."
+    end
+    alert_message
+  end
+
+  def available_post_to_group_ids
+    list_groups_active.map(&:id) & post_to_group_ids
+  end
+
+  def post_to_group_ids
+    in_group_ids.split(",").map(&:to_i)
   end
 
   private
 
+  def list_groups_active
+    @list_groups_active ||= Member::ListGroup.new(@creator).active
+  end
+
+  def add_poll_to_feed!
+    @poll.poll_members.create!({
+      member: @creator,
+      share_poll_of_id: 0,
+      series: false,
+      public: poll_public_status,
+      expire_date: convert_expire_date,
+      in_group: in_group_status
+    })
+  end
+
+  def query_groups
+    @query_groups ||= Group.where(id: available_post_to_group_ids)
+  end
+
+  def add_poll_to_group!
+    query_groups.each do |group|
+      group.poll_groups.create!({
+        poll: @poll,
+        member: @creator
+      })
+    end
+  end
+
+  def add_poll_to_company!
+    PollCompany.create_poll(@poll, @creator.get_company, :mobile)
+  end
+
+  def tracking_activtiy_feed
+    Company::TrackActivityFeedPoll.new(@creator, in_group_ids, @poll, "create").tracking
+  end
+
+  def decrease_point!
+    @creator.with_lock do
+      @creator.point -= 1
+      @creator.save!
+    end
+  end
+
+  def creatable_tagging!
+    if split_tags_form_title.size > 0
+      split_tags_form_title.each do |tag_name|
+        @poll.tags << Tag.where(name: tag_name).first_or_create!
+      end
+    end
+  end
+
+  def watching_poll!
+    WatchPoll.new(@creator, @poll.id).watching
+  end
+
   def poll_public_status
-    default_public = false
-    default_public = params[:is_public] if params[:is_public].present?
-    return default_public
+    if params[:is_public]
+      true
+    else
+      false
+    end
   end
 
   def in_group_status
@@ -97,7 +214,7 @@ class CreatePollService
 
   def convert_expire_date
     expire_date_default = Time.zone.now + 100.years
-    expire_date_default = Time.zone.now + params[:expire_date].to_i.days if params[:expire_date].present?
+    expire_date_default = Time.zone.now + params[:expire_within].to_i.days if params[:expire_within].present?
     return expire_date_default
   end
 
@@ -106,6 +223,20 @@ class CreatePollService
       qrcode_key = SecureRandom.hex(6)
     end while Poll.exists?(qrcode_key: qrcode_key)
     qrcode_key
+  end
+
+  def additional_poll_attachments
+    new_original_images = params[:original_images].collect!{ |image_url| ImageUrl.new(image_url).split_cloudinary_url }
+
+    new_original_images.each_with_index do |url_attachment, index|
+      poll_attachment = poll_attachments.create!(order_image: index + 1)
+      poll_attachment.update_column(:image, url_attachment)
+    end
+  end
+
+  def update_photo_poll
+    init_image_url = ImageUrl.new(params[:photo_poll])
+    @poll.update_column(:photo_poll, init_image_url.split_cloudinary_url)
   end
 
 end
--- test/models/create_poll_service_test.rb
+++ test/models/create_poll_service_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class CreatePollServiceTest < ActiveSupport::TestCase
 
   def setup
-    @poll = CreatePollService.new(members(:one), {})
+    params = {
+      title: "Mockup",
+      choices: ["1", "2"]
+    }
+    @poll = CreatePollService.new(members(:one), params)
   end
 
   def test_that_poll_not_in_public_by_default
@@ -54,8 +58,41 @@ class CreatePollServiceTest < ActiveSupport::TestCase
     assert_equal(@poll.calculate_choices, 2)
   end
 
-  def test_that_it_have_a_default_type_poll_is_freeform
-    assert_equal(@poll.default_type_poll, "freeform")
+  def test_that_it_have_a_type_poll_default_is_freeform
+    assert_equal(@poll.set_type_poll, "freeform")
+  end
+
+  def test_that_it_have_a_qrcode_key
+    assert @poll.poll_attributes[:qrcode_key].present?
+  end
+
+  def test_that_it_have_a_show_result_is_true
+    assert @poll.poll_attributes[:show_result]
+  end
+
+  def test_that_it_have_a_qr_only_is_false
+    assert_not @poll.poll_attributes[:qr_only]
+  end
+
+  def test_that_it_have_a_require_info_is_false
+    assert_not @poll.poll_attributes[:require_info]
+  end
+
+  def test_that_it_have_a_priority_is_zero
+    assert_equal(@poll.new_poll.priority, 0)
+  end
+
+  def test_that_it_have_tags_from_title
+    @poll.params = {
+      title: "wow #nut #coding"
+    }
+
+    assert_equal(@poll.split_tags_form_title, ["nut", "coding"])
+  end
+
+  def test_that_it_have_to_watching_poll
+    poll = @poll.create!
+    assert_equal(poll.watcheds.count, 1)
   end
 
 end
