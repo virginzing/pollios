ea0464f285eb8193bb22dcde646989ebfa93329e
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-23 14:18:01 +0700

    add more test to create_poll_service

--- app/services/create_poll_service.rb
+++ app/services/create_poll_service.rb
@@ -1,7 +1,7 @@
 class CreatePollService
   include PollsHelper
 
-  attr_accessor :params
+  attr_accessor :params, :creator
 
   def initialize(creator, params)
     @creator = creator
@@ -47,7 +47,7 @@ class CreatePollService
   end
 
   def is_citizen_and_having_point?
-    @creator.citizen? && @creator.point > 0
+    creator.citizen? && creator.point > 0
   end
 
   def split_tags_form_title
@@ -57,7 +57,7 @@ class CreatePollService
   end
 
   def poll_attributes
-    params[:member_id] = @creator.id
+    params[:member_id] = creator.id
     params[:expire_date] = convert_expire_date
     params[:public] = poll_public_status
     params[:in_group] = in_group_status
@@ -68,7 +68,7 @@ class CreatePollService
     params[:campaign_id] = 0
     params[:recurring_id] = 0
     params[:qrcode_key] = generate_qrcode_key
-    params[:member_type] = @creator.member_type_text
+    params[:member_type] = creator.member_type_text
     params[:show_result] = true
     params[:require_info] = false
     params[:qr_only] = false
@@ -93,11 +93,11 @@ class CreatePollService
         watching_poll!
         add_poll_to_feed!
         add_poll_to_group! if in_group?
-        add_poll_to_company! if @creator.company?
+        add_poll_to_company! if creator.company?
         tracking_activtiy_feed! if in_group?
-        MemberActiveRecord.record_member_active(@creator)
+        MemberActiveRecord.record_member_active(creator)
         PollStats.create_poll_stats(@poll)
-        Activity.create_activity_poll(@creator, @poll, 'Create')
+        Activity.create_activity_poll(creator, @poll, 'Create')
         @poll
       else
         raise ExceptionHandler::UnprocessableEntity, @poll.errors.full_messages.join(", ")
@@ -109,14 +109,15 @@ class CreatePollService
     if in_public?
       raise ExceptionHandler::UnprocessableEntity, ExceptionHandler::Message::Poll::POINT_ZERO unless is_citizen_and_having_point?
     else
-      if in_group? && !@creator.company?
+      if in_group? && !creator.company?
         unless available_post_to_group_ids.size > 0
           group_names = Group.where(id: post_to_group_ids)
-          alert_message = "You're no longer in #{group_names.map(&:name).join(', ')}"
+          alert_message = "You're no longer in #{group_names.map(&:name).join(', ')}."
           raise ExceptionHandler::CustomError, AlertSerializer.new({ response_status: "OK", alert_message: alert_message}).to_json
         end
       end
     end
+    true
   end
 
   def alert_message
@@ -135,15 +136,19 @@ class CreatePollService
     in_group_ids.split(",").map(&:to_i)
   end
 
+  def convert_list_original_images
+    params[:original_images].collect!{ |image_url| ImageUrl.new(image_url).split_cloudinary_url }
+  end
+
   private
 
   def list_groups_active
-    @list_groups_active ||= Member::ListGroup.new(@creator).active
+    @list_groups_active ||= Member::ListGroup.new(creator).active
   end
 
   def add_poll_to_feed!
     @poll.poll_members.create!({
-      member: @creator,
+      member: creator,
       share_poll_of_id: 0,
       series: false,
       public: poll_public_status,
@@ -160,23 +165,23 @@ class CreatePollService
     query_groups.each do |group|
       group.poll_groups.create!({
         poll: @poll,
-        member: @creator
+        member: creator
       })
     end
   end
 
   def add_poll_to_company!
-    PollCompany.create_poll(@poll, @creator.get_company, :mobile)
+    PollCompany.create_poll(@poll, creator.get_company, :mobile)
   end
 
-  def tracking_activtiy_feed
-    Company::TrackActivityFeedPoll.new(@creator, in_group_ids, @poll, "create").tracking
+  def tracking_activtiy_feed!
+    Company::TrackActivityFeedPoll.new(creator, in_group_ids, @poll, "create").tracking
   end
 
   def decrease_point!
-    @creator.with_lock do
-      @creator.point -= 1
-      @creator.save!
+    creator.with_lock do
+      creator.point -= 1
+      creator.save!
     end
   end
 
@@ -189,7 +194,7 @@ class CreatePollService
   end
 
   def watching_poll!
-    WatchPoll.new(@creator, @poll.id).watching
+    WatchPoll.new(creator, @poll.id).watching
   end
 
   def poll_public_status
@@ -226,9 +231,7 @@ class CreatePollService
   end
 
   def additional_poll_attachments
-    new_original_images = params[:original_images].collect!{ |image_url| ImageUrl.new(image_url).split_cloudinary_url }
-
-    new_original_images.each_with_index do |url_attachment, index|
+    convert_list_original_images.each_with_index do |url_attachment, index|
       poll_attachment = poll_attachments.create!(order_image: index + 1)
       poll_attachment.update_column(:image, url_attachment)
     end
--- test/models/create_poll_service_test.rb
+++ test/models/create_poll_service_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 class CreatePollServiceTest < ActiveSupport::TestCase
 
   def setup
-    params = {
+    @member = members(:one)
+    @poll = CreatePollService.new(@member, {
       title: "Mockup",
       choices: ["1", "2"]
-    }
-    @poll = CreatePollService.new(members(:one), params)
+    })
+    @group = groups(:one)
+    @group_two = groups(:two)
   end
 
   def test_that_poll_not_in_public_by_default
@@ -15,17 +17,13 @@ class CreatePollServiceTest < ActiveSupport::TestCase
   end
 
   def test_that_poll_in_public
-    @poll.params = {
-      is_public: true
-    }
+    @poll.params[:is_public] = true
 
     assert @poll.in_public?
   end
 
   def test_that_poll_not_in_public
-    @poll.params = {
-      is_public: false
-    }
+    @poll.params[:is_public] = false
 
     assert_not @poll.in_public?
   end
@@ -35,25 +33,19 @@ class CreatePollServiceTest < ActiveSupport::TestCase
   end
 
   def test_that_poll_in_group
-    @poll.params = {
-      group_id: "1,2"
-    }
+    @poll.params[:group_id] = "1,2"
 
     assert @poll.in_group?
   end
 
   def test_that_it_have_a_list_choices
-    @poll.params = {
-      choices: ["A", "B"]
-    }
+    @poll.params[:choices] = ["A", "B"]
 
     assert_equal(@poll.list_choices, ["A", "B"])
   end
 
   def test_that_it_have_two_choices_count
-    @poll.params = {
-      choices: ["A", "B"]
-    }
+    @poll.params[:choices] = ["A", "B"]
 
     assert_equal(@poll.calculate_choices, 2)
   end
@@ -83,9 +75,7 @@ class CreatePollServiceTest < ActiveSupport::TestCase
   end
 
   def test_that_it_have_tags_from_title
-    @poll.params = {
-      title: "wow #nut #coding"
-    }
+    @poll.params[:title] = "wow #nut #coding"
 
     assert_equal(@poll.split_tags_form_title, ["nut", "coding"])
   end
@@ -94,5 +84,106 @@ class CreatePollServiceTest < ActiveSupport::TestCase
     poll = @poll.create!
     assert_equal(poll.watcheds.count, 1)
   end
+
+  def test_that_validate_success_when_post_poll_to_friend
+    @poll.params[:type_poll] = "binary"
+    @poll.params[:is_public] = false
+
+    assert @poll.validate_that_can_create!
+  end
+
+  def test_that_alert_message_is_nil_when_not_post_to_group
+    @poll.params[:type_poll] = "binary"
+    @poll.params[:is_public] = false
+
+    assert_nil @poll.alert_message
+  end
+
+  def test_that_available_post_to_group_ids_when_nost_post_to_group
+    @poll.params[:type_poll] = "binary"
+    @poll.params[:is_public] = false
+    assert_empty @poll.available_post_to_group_ids
+  end
+
+  def test_that_post_to_group_ids
+    @poll.params[:group_id] = "1,2"
+    assert_equal(@poll.post_to_group_ids, [1,2])
+  end
+
+  def test_that_thumnail_type_default_is_0
+    assert_equal(@poll.poll_attributes[:thumbnail_type], 0)
+  end
+
+  def test_that_convert_list_original_images
+    @poll.params[:original_images] =  ["http://res.cloudinary.com/code-app/image/upload/v1436275533/mkhzo71kca62y9btz3bd.png",
+                        "http://res.cloudinary.com/code-app/image/upload/v1433344706/dcxanj8qcc1fal6r0rud.png"]
+
+    assert_equal(@poll.convert_list_original_images, ["v1436275533/mkhzo71kca62y9btz3bd.png", "v1433344706/dcxanj8qcc1fal6r0rud.png"])
+  end
+
+  def test_class_image_url
+    @image_url = ImageUrl.new("http://res.cloudinary.com/code-app/image/upload/v1436275533/mkhzo71kca62y9btz3bd.png")
+
+    assert_equal(@image_url.split_cloudinary_url, "v1436275533/mkhzo71kca62y9btz3bd.png")
+  end
+
+  def test_when_post_poll_to_public_with_default_value
+    @poll.params[:is_public] = true
+
+    assert @poll.validate_that_can_create!
+    assert @poll.in_public?
+    assert @poll.new_poll.public
+    assert @poll.create!
+  end
+
+  def test_that_add_poll_to_feed_when_poll_create_public_or_friend
+    poll = @poll.create!
+
+    assert poll.poll_members.present?
+  end
+
+  def test_that_add_poll_to_group_when_poll_create_to_group
+    @poll.params[:group_id] = @group.id.to_s
+    poll = @poll.create!
+
+    assert poll.poll_groups.present?
+  end
+
+  def test_that_add_poll_group_but_no_longer_this_group
+    @poll.params[:group_id] = @group_two.id.to_s
+    exception = assert_raise(ExceptionHandler::CustomError) { @poll.create! }
+
+    alert_message = "You're no longer in second group."
+    assert_equal(AlertSerializer.new({ response_status: "OK", alert_message: alert_message}).to_json[:alert_message], eval(exception.message)[:alert_message])
+  end
+
+  def test_add_poll_to_more_group_but_no_longer_some_group
+    @poll.params[:group_id] = [@group.id, @group_two.id].join(",")
+
+    assert_equal(@poll.available_post_to_group_ids, [@group.id])
+    assert @poll.create!
+    assert_equal(@poll.alert_message, "This poll don't show in second group because you're no longer these group.")
+  end
+
+  def test_that_citizen_have_a_point_when_post_to_pubic
+    @poll.params[:is_public] = true
+    p @poll.available_post_to_group_ids
+    assert @poll.create!
+  end
+
+  def test_that_citizen_is_not_a_point_when_post_to_public
+    @poll.creator = members(:citizen_no_point)
+    @poll.params[:is_public] = true
+
+    exception = assert_raise(ExceptionHandler::UnprocessableEntity) { @poll.create! }
+    assert_equal(ExceptionHandler::Message::Poll::POINT_ZERO, exception.message)
+  end
+
+  def test_add_poll_with_1_image
+    image_url = "http://res.cloudinary.com/code-app/image/upload/v1436275533/mkhzo71kca62y9btz3bd.png"
+    @poll.params[:photo_poll] = image_url
+    poll = @poll.create!
+  end
+
 
 end
