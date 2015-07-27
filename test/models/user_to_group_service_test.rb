7519e61cb3428c24dbcfb1800ac8d927fbc613f8
Nuttapon Achachotipong <nuttapon.achachotipong@gmail.com>
2015-07-27 11:24:03 +0700

    add more test to user_to_group

--- test/models/user_to_group_service_test.rb
+++ test/models/user_to_group_service_test.rb
@@ -6,17 +6,27 @@ class UserToGroupServiceTest < ActiveSupport::TestCase
     @member = members(:one)
     @member_two = members(:two)
     @group = groups(:two)
-  end
 
-  def test_that_search_users
-    init = UserToGroupService.new({
+    @init = UserToGroupService.new({
       member_id: @member.id,
       group_id: @group.id,
       fullname: "w"
     })
+  end
 
-    assert init.users.present?
-    assert_equal(init.users.size, 1)
+  def test_that_search_users
+    assert @init.users.present?
+    assert_equal(@init.users.size, 1)
+  end
+
+  def test_that_add_user_to_group
+    assert @init.add!
+  end
+
+  def test_that_create_group_member
+    assert_difference "GroupMember.count", 1 do
+      @init.add!
+    end
   end
 
 end
