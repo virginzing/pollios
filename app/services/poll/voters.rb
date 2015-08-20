b57c46de73b08e9720a5274f55ddbf61c96a3e19
Rawitat Pulam <rawitat@code-app.com>
2015-08-20 12:17:18 +0700

    changed LIMIT_MEMBER in application.yml 500=>1000. changed coding style in poll/voters.rb

--- app/services/poll/voters.rb
+++ app/services/poll/voters.rb
@@ -4,10 +4,10 @@ class Poll::Voters
     end
 
     def members
+        result = []
         if @params[:choice_id]
-            Member.joins(:history_votes).where("history_votes.choice_id IN (?)", @params[:choice_id])
-        else
-            []
+            result = Member.joins(:history_votes).where("history_votes.choice_id IN (?)", @params[:choice_id])
         end
+        return result
     end
 end
