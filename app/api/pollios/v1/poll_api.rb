0f18be9b3f7c3312a905a8e849f6d69d4a6b15ec
Rawitat Pulam <rawitat@code-app.com>
2015-09-27 13:39:55 +0700

    reindented

--- app/api/pollios/v1/poll_api.rb
+++ app/api/pollios/v1/poll_api.rb
@@ -1,14 +1,14 @@
 module Pollios::V1
-    class PollAPI < Grape::API
-        version 'v1', using: :path
+  class PollAPI < Grape::API
+    version 'v1', using: :path
 
-        desc 'Hello World!!!'
-        params do
-            requires :message, type: String
-        end
-
-        get :say do
-            { m: params[:message] }
-        end 
+    desc 'Hello World!!!'
+    params do
+      requires :message, type: String
     end
+
+    get :say do
+      { m: params[:message] }
+    end 
+  end
 end
