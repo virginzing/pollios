beead8d37b0361f44b54923bfbca7dc22760d280
Rawitat Pulam <rawitat@code-app.com>
2015-09-29 13:07:26 +0700

    starting to make proper new v1 api on grape. with a few todos. removed ex say method

--- app/api/pollios/v1/poll_api.rb
+++ app/api/pollios/v1/poll_api.rb
@@ -2,19 +2,40 @@ module Pollios::V1
   class PollAPI < Grape::API
     version 'v1', using: :path
 
-    desc 'Hello World!!!'
-    params do
-      # requires :message, type: String
+    #TODO: Factorize this into proper API helper
+    helpers do
+
+      #TODO: properly authenticate this
+      def current_user
+        @current_user = nil
+      end
+
+      def authenticate!
+        error!('401 Unauthorized', 401) unless current_user
+      end
     end
 
     resource :polls do
       get :all_count do
         { count: Poll.count }
       end
+
+      desc "return: requesting member's home timeline"
+      get :personal_timeline do
+      end
+
+      desc "return: poll detail for requesting member"
+      params do
+        requires :id, type: Integer, desc: "poll id" 
+      end
+      route_param :id do
+        get do
+          authenticate!
+          { id:  Poll.find(params[:id]) }
+        end
+      end
+
     end
 
-    get :say do
-      # { m: params[:message] }
-    end 
   end
 end
