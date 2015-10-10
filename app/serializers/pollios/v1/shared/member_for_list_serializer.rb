7c123ca5f62be858395ac19517caa63763e1f6bc
Rawitat Pulam <rawitat@code-app.com>
2015-10-10 16:51:33 +0700

    avatar url now working. not sure if correctly or not

--- app/serializers/pollios/v1/shared/member_for_list_serializer.rb
+++ app/serializers/pollios/v1/shared/member_for_list_serializer.rb
@@ -2,10 +2,15 @@ module Pollios::V1::Shared
   class MemberForListSerializer < ActiveModel::Serializer
     delegate :current_member, to: :scope
 
-    attributes :id, :fullname, :status
+    attributes :id, :fullname, :avatar, :description, :key_color
+    attributes :status
 
     def status
       object.is_friend(current_member)
+    end
+
+    def avatar
+      object.get_avatar
     end
   end
 end
