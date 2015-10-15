561852bca1a99981c1b64ff34d396a506853dbf0
Rawitat Pulam <rawitat@code-app.com>
2015-10-15 17:02:46 +0700

    added white space

--- app/entities/pollios/v1/shared/group_for_list_entity.rb
+++ app/entities/pollios/v1/shared/group_for_list_entity.rb
@@ -6,8 +6,8 @@ module Pollios::V1::Shared
     expose :member_count
     expose :name
     expose :description, if: -> (object, options) { object.description.present? }
-    expose :get_cover_group, as: :cover, if: -> (object, options) {object.get_cover_group.present? }
-    expose :cover_preset, unless: -> (object, options) {object.get_cover_group.present? }
+    expose :get_cover_group, as: :cover, if: -> (object, options) { object.get_cover_group.present? }
+    expose :cover_preset, unless: -> (object, options) { object.get_cover_group.present? }
 
     def member_count
       Group::ListMember.new(object).active.count
