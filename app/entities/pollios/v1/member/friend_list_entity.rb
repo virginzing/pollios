e01f8c1237f6bfaaf6925c8f4992ba726daecffc
Rawitat Pulam <rawitat@code-app.com>
2015-10-15 13:48:03 +0700

    added followers, followings, and blocks to friends api

--- app/entities/pollios/v1/member/friend_list_entity.rb
+++ app/entities/pollios/v1/member/friend_list_entity.rb
@@ -5,6 +5,18 @@ module Pollios::V1::Member
       Pollios::V1::Shared::MemberForListEntity.represent object.friends, current_member_linkage: current_member_linkage
     end
 
+    expose :followers do |object, options|
+      Pollios::V1::Shared::MemberForListEntity.represent object.followers, current_member_linkage: current_member_linkage
+    end
+
+    expose :followings do |object, options|
+      Pollios::V1::Shared::MemberForListEntity.represent object.followings, current_member_linkage: current_member_linkage
+    end
+
+    expose :blocks do |object, options|
+      Pollios::V1::Shared::MemberForListEntity.represent object.blocks, current_member_linkage: current_member_linkage
+    end
+
     def current_member_linkage
       current_member = options[:current_member]
       Member::MemberList.new(current_member).social_linkage_ids
