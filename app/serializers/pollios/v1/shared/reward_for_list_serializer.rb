c95c6df053b5558d384df14333331555c8627d47
Rawitat Pulam <rawitat@code-app.com>
2015-10-15 16:01:55 +0700

    group-for-list-entity now using shorthanded lambda instead of proc

--- app/serializers/pollios/v1/shared/reward_for_list_serializer.rb
+++ app/serializers/pollios/v1/shared/reward_for_list_serializer.rb
@@ -1,7 +1,7 @@
 module Pollios::V1::Shared
 	class RewardForListSerializer < Pollios::V1::BaseSerializer
 
-		attributes :campiagn_detail, :reward_info
+		attributes :campaign_detail, :reward_info
 
 		def initialize(object, options = {})
 			super(object, options )
@@ -9,40 +9,40 @@ module Pollios::V1::Shared
 			@campaign = object.campaign
 		end
 
-		def campiagn_detail
+		def campaign_detail
 			{
-      			id: @campaign.id,
-      			name: @campaign.name.presence || "",
-      			description: @campaign.description.presence || "",
-      			how_to_redeem: @campaign.how_to_redeem.presence || "",
-      			expire: @campaign.expire.to_i,
-      			photo_campaign: @campaign.get_photo_campaign,
-      			original_photo_campaign: @campaign.get_original_photo_campaign,
-      			used: @campaign.used,
-      			limit: @campaign.limit,
-      			owner_info: @campaign.member.present? ? MemberInfoFeedSerializer.new(@campaign.member) : System::DefaultMember.new.to_json,
-      			created_at: @campaign.created_at.to_i,
-      			type_campaign: @campaign.type_campaign,
-     			announce_on: @campaign.announce_on.to_i,
-      			random_reward: @campaign.begin_sample == @campaign.end_sample ? false : true
-    		}
-		end
+        id: @campaign.id,
+        name: @campaign.name.presence || "",
+        description: @campaign.description.presence || "",
+        how_to_redeem: @campaign.how_to_redeem.presence || "",
+        expire: @campaign.expire.to_i,
+        photo_campaign: @campaign.get_photo_campaign,
+        original_photo_campaign: @campaign.get_original_photo_campaign,
+        used: @campaign.used,
+        limit: @campaign.limit,
+        owner_info: @campaign.member.present? ? MemberInfoFeedSerializer.new(@campaign.member) : System::DefaultMember.new.to_json,
+        created_at: @campaign.created_at.to_i,
+        type_campaign: @campaign.type_campaign,
+        announce_on: @campaign.announce_on.to_i,
+        random_reward: @campaign.begin_sample == @campaign.end_sample ? false : true
+      }
+    end
 
-		def reward_info
-			{
-      			id: object.id,
-      			reward_status: object.reward_status,
-      			serial_code: object.serial_code || "",
-      			redeem: object.redeem,
-      			redeem_at: object.redeem_at.to_i.presence || "",
-      			ref_no: object.ref_no || "",
-      			created_at: object.created_at.to_i,
-      			title: @campaign.get_reward_title,
-      			detail: @campaign.get_reward_detail,
-      			expire: @campaign.get_reward_expire,
-      			redeem_myself: @campaign.redeem_myself
-    		}
-		end
+    def reward_info
+     {
+       id: object.id,
+       reward_status: object.reward_status,
+       serial_code: object.serial_code || "",
+       redeem: object.redeem,
+       redeem_at: object.redeem_at.to_i.presence || "",
+       ref_no: object.ref_no || "",
+       created_at: object.created_at.to_i,
+       title: @campaign.get_reward_title,
+       detail: @campaign.get_reward_detail,
+       expire: @campaign.get_reward_expire,
+       redeem_myself: @campaign.redeem_myself
+     }
+   end
 
-	end
+ end
 end
