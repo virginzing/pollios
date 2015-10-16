40f490bff12f6c5cbb78d1fab7772181c97524e8
zingfox <zingfox@hotmail.com>
2015-10-16 15:22:29 +0700

    refactor members/:id/rewards api

---
+++ app/entities/pollios/v1/shared/campaign_owner_detail_entity.rb
@@ -0,0 +1,22 @@
+module Pollios::V1::Shared
+	class CampaignOwnerDetailEntity < Grape::Entity
+
+		expose :id, as: :member_id
+		expose :member_type_text, as: :type
+		expose :fullname, as: :name
+		expose :description do |object|
+			object.description.to_s
+		end
+		expose :avatar do |object|
+			object.avatar.present? ? resize_avatar(object.avatar.url) : ""
+		end
+		expose :key_color do |object|
+			object.key_color.to_s
+		end
+		
+		def resize_avatar(avatar_url)
+    	avatar_url.split("upload").insert(1, "upload/c_fill,h_180,w_180," + Cloudinary::QualityImage::SIZE).sum
+  	end
+
+	end
+end
--- app/entities/pollios/v1/shared/member_detail_entity.rb
+++
@@ -1,8 +0,0 @@
-module Pollios::V1::Shared
-	class MemberDetailEntity < Grape::Entity
-
-		expose :id, as: :member_id
-		expose :member_type_text, as: :type
-
-	end
-end
