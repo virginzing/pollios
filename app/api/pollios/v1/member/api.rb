f742ada4dce917ee444045b0eb6bcad16125fa76
Rawitat Pulam <rawitat@code-app.com>
2015-10-20 16:12:54 +0700

    refactored notifications api. notification list no longer in route :id space

--- app/api/pollios/v1/member/api.rb
+++ app/api/pollios/v1/member/api.rb
@@ -2,33 +2,46 @@ module Pollios::V1::Member
   class API < Grape::API
     version 'v1', using: :path
 
+    helpers do
+      def member
+        @member ||= Member.cached_find(params[:id])
+      end
+
+      def verify_viewing_member_right
+        error!("403 Forbidden: not allowing this operation on other member", 403) unless member.id == current_member.id
+      end
+    end
+
     resource :members do
+
+      desc "returns list of member's notifications"
+      resource :notifications do
+        params do
+          optional :page_index, type: Integer, desc: "page index for notification's pagination"
+          optional :clear_new_count, type: Boolean, desc: "should clear member's new notification count"
+        end
+        get do
+          notifications_for_member = Member::NotificationList.new(current_member, {:page_index => params[:page_index], :clear_new_count => params[:clear_new_count]})
+          present notifications_for_member, with: Pollios::V1::Member::NotificationListEntity
+        end
+      end
+
 
       params do
         requires :id, type: Integer, desc: "member id"
-      end
-
-      before do
-        @member = Member.cached_find(params[:id])
-      end
-
-      helpers do
-        def verify_viewing_member_right
-          error!("403 Forbidden: not allowing this operation on other member", 403) unless @member.id == current_member.id
-        end
       end
 
       route_param :id do
 
         desc "returns member detail for profile screen of member"
         get do
-          { member: @member }
+          { member: member }
         end
 
         desc "returns list of member's friends & followings"
         resource :friends do
           get do
-             friends_of_member = Member::MemberList.new(@member)
+             friends_of_member = Member::MemberList.new(member)
              present friends_of_member, with: Pollios::V1::Member::FriendListEntity, current_member: current_member
           end
         end
@@ -37,7 +50,7 @@ module Pollios::V1::Member
         resource :groups do
           get do
             options = {:viewing_member => current_member }
-            groups_for_member = Member::GroupList.new(@member, options)
+            groups_for_member = Member::GroupList.new(member, options)
             present groups_for_member, with: Pollios::V1::Member::GroupListEntity, current_member: current_member
           end
         end
@@ -45,25 +58,14 @@ module Pollios::V1::Member
         desc "returns list of member's rewards"
         resource :rewards do
           get do
-            rewards_of_member = Member::RewardList.new(@member)
+            rewards_of_member = Member::RewardList.new(member)
             present rewards_of_member, with: Pollios::V1::Member::RewardListEntity, current_member: current_member
           end
         end
-        
-        desc "returns list of member's notifications"
-        resource :notifications do
-          params do
-            optional :page_index, type: Integer, desc: "page index for notification's pagination"
-            optional :clear_new_count, type: Boolean, desc: "should clear member's new notification count"
-          end
-          get do
-            verify_viewing_member_right
-            notifications_for_member = Member::NotificationList.new(@member, {:page_index => params[:page_index], :clear_new_count => params[:clear_new_count]})
-            present notifications_for_member, with: Pollios::V1::Member::NotificationListEntity, current_member: current_member
-          end
-        end
-      end 
-    end 
 
-  end
+      end
+
+    end
+
+  end 
 end 
