3edae4449f4dd013ff093e06135d369a7567d79d
zingfox <zingfox@hotmail.com>
2015-10-29 11:34:53 +0700

    changed key 'page_index' in notification api to 'next_notifications'

--- app/entities/pollios/v1/current_member/notification_list_entity.rb
+++ app/entities/pollios/v1/current_member/notification_list_entity.rb
@@ -1,7 +1,8 @@
 module Pollios::V1::CurrentMember
   class NotificationListEntity < Pollios::V1::BaseEntity
     expose :notifications_count, as: :total_entries
-    expose :next_page_index
+    
+    expose :next_notifications
 
     expose :notifications_at_current_page, as: :notifications, with: NotificationEntity
   end
