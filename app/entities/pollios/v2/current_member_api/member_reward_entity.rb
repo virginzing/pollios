f41fac7c78f7b8c618128d4afb0334b3e306897b
Rawitat Pulam <rawitat@code-app.com>
2015-11-07 01:12:48 +0700

    new response logic/layout for new v2 reward list api, as discussed today (not completed)

--- app/entities/pollios/v2/current_member_api/member_reward_entity.rb
+++ app/entities/pollios/v2/current_member_api/member_reward_entity.rb
@@ -1,21 +1,30 @@
 module Pollios::V2::CurrentMemberAPI
   class MemberRewardEntity < Pollios::V2::BaseEntity
 
-    expose :redeemable_info do
-      expose :id, as: :redeem_id, if: -> (obj, opts) { obj.received? }
+    expose :reward_status, as: :status
+
+    # if user got reward, show redeemable info, along with rewarad
+    expose :redeemable_info, if: -> (obj, opts) { obj.received? } do
+      expose :id, as: :redeem_id
       expose :reward_status, as: :status
-      expose :serial_code, if: -> (obj, opts) { obj.received? }
-      expose :redeem, as: :claimed, if: -> (obj, opts) { obj.received? }
+      expose :serial_code
+      expose :redeem, as: :claimed
 
       with_options(format_with: :as_integer) do
-        expose :redeem_at, as: :claimed_at, if: -> (obj, opts) { obj.received? }
+        expose :redeem_at, as: :claimed_at
       end
 
       expose :ref_no, if: -> (obj, opts) { obj.received? }
+
     end
 
-    expose :reward, with: Pollios::V2::Shared::RewardEntity
+    expose :reward, with: Pollios::V2::Shared::RewardEntity, if: -> (obj, opts) { obj.received? }
 
+    # if user not getting reward or still waiting annoucement, showing campaign info
+    expose :campaign, if: -> (obj, opts) { !obj.received? } do
+    end
+
+    # always show poll if poll information present
     expose :poll, if: -> (obj, opts) { obj.poll.present? } do |obj|
       Pollios::V1::PollAPI::PollDetailEntity.represent obj.poll, only: [:poll_id, :title]
     end
