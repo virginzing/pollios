f0dad28c6df53a674d3b5a83528559f6288a814a
Rawitat Pulam <rawitat@code-app.com>
2015-11-16 23:30:45 +0700

    refactored status hash function. changed if-else to case-when.

--- app/entities/pollios/v1/shared/member_entity.rb
+++ app/entities/pollios/v1/shared/member_entity.rb
@@ -1,49 +1,49 @@
 module Pollios::V1::Shared
   class MemberEntity < Pollios::V1::BaseEntity
-    
+
     expose :id, as: :member_id
     expose :fullname, as: :name
     expose :description
     expose :get_avatar, as: :avatar
     expose :member_type_text, as: :type
-    expose :get_key_color, as: :key_color, if: -> (obj, opts) { obj.get_key_color.present? }
-    expose :status, if: -> (object, options) { options[:current_member_linkage].present? }
+    expose :get_key_color, as: :key_color, if: -> (obj, _) { obj.get_key_color.present? }
+    expose :status, if: -> (_, opts) { opts[:current_member_linkage].present? }
 
     def self.default_pollios_member
       {
         member_id: 0,
-        name: "Pollios System",
-        avatar: "http://pollios.com/images/logo/pollios_system_notification.png",
-        description: "Pollios Office"
+        name: 'Pollios System',
+        avatar: 'http://pollios.com/images/logo/pollios_system_notification.png',
+        description: 'Pollios Office'
       }
     end
 
     def status
-      relation = options[:current_member_linkage]
-
-      hash = { :status => :nofriend }
-
-      is_friend = relation[:friends_ids].include?(object.id)
-      is_requesting = relation[:requesting_ids].include?(object.id)
-      is_being_requested = relation[:being_requested_ids].include?(object.id)
-      is_blocking = relation[:blocks_ids].include?(object.id)
-
-      if is_friend
-        hash[:status] = :friend
-      elsif is_requesting
-        hash[:status] = :invite
-      elsif is_being_requested
-        hash[:status] = :invitee
-      elsif is_blocking
-        hash[:status] = :block
-      end
+      hash = { status: hash_status }
 
       if object.celebrity? || object.brand? || object.company?
+        relation = options[:current_member_linkage]
         is_following = relation[:followings_ids].include?(object.id)
         hash[:following] = is_following ? true : false
       end
 
       hash
+    end
+
+    private def hash_status
+      relation = options[:current_member_linkage]
+      case 
+      when relation[:friends_ids].include?(object.id)
+        :friend
+      when relation[:requesting_ids].include?(object.id)
+        :invite
+      when relation[:being_requested_ids].include?(object.id)
+        :invitee
+      when relation[:blocks_ids].include?(object.id)
+        :block
+      else
+        :nofriend
+      end
     end
 
   end
