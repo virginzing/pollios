f77133aa53e201a0182d9c671a4168ddf77dd131
Rawitat Pulam <rawitat@code-app.com>
2015-11-17 16:28:34 +0700

    added unprocessable entity rescuer from rails-stack into grape api-root

--- app/api/pollios/api.rb
+++ app/api/pollios/api.rb
@@ -4,7 +4,7 @@ module Pollios
     prefix :api
 
     before do
-      error!("401 Unauthorized", 401) unless current_member
+      error!('401 Unauthorized', 401) unless current_member
     end
 
     helpers do
@@ -12,10 +12,10 @@ module Pollios
       def authenticate_member
         member = member_if_allowed
         authen_token = headers['X-Api-Key']
-        error!("401 Unauthorized: no token", 401) unless authen_token.present?
+        error!('401 Unauthorized: no token', 401) unless authen_token.present?
 
         member_access_token = member.api_tokens.where('token = ?', authen_token)
-        error!("401 Unauthorized: invalid token", 401) unless member_access_token.present?
+        error!('401 Unauthorized: invalid token', 401) unless member_access_token.present?
 
         member
       end
@@ -26,11 +26,16 @@ module Pollios
 
       def member_if_allowed
         member = Member.cached_find(params[:member_id])
-        error!("403 Forbidden", 403) if member.blacklist? || member.ban?
+        error!('403 Forbidden', 403) if member.blacklist? || member.ban?
         member
       end
 
     end
+
+    rescue_from ExceptionHandler::UnprocessableEntity do |e|
+      error!(e.message, 422)
+    end
+
 
     mount V1::API
     mount V2::API
