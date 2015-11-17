9dd77b4277010cd7337eef1664e7122f28fe287b
Rawitat Pulam <rawitat@code-app.com>
2015-11-17 17:10:14 +0700

    rubocop-beautified current_member_api

--- app/api/pollios/v1/current_member_api/get.rb
+++ app/api/pollios/v1/current_member_api/get.rb
@@ -11,7 +11,8 @@ module Pollios::V1::CurrentMemberAPI
           optional :clear_new_count, type: Boolean, desc: "should clear member's new notification count"
         end
         get do
-          notifications_for_member = Member::NotificationList.new(current_member, {:index => params[:index], :clear_new_count => params[:clear_new_count]})
+          options = { index: params[:index], clear_new_count: params[:clear_new_count] }
+          notifications_for_member = Member::NotificationList.new(current_member, options)
           present notifications_for_member, with: NotificationListEntity
         end
       end
@@ -19,13 +20,13 @@ module Pollios::V1::CurrentMemberAPI
       desc "returns list of current member's rewards"
       resource :rewards do
         get do
-          rewards_of_member = Member::RewardList.new(current_member, {:page_index => params[:page_index]})
+          rewards_of_member = Member::RewardList.new(current_member, page_index: params[:page_index])
           present rewards_of_member, with: RewardListEntity
         end
 
-        desc "returns reward at id for current member"
+        desc 'returns reward at id for current member'
         params do
-          requires :id, type: Integer, desc: "reward id" 
+          requires :id, type: Integer, desc: 'reward id'
         end
         route_param :id do
           get do
@@ -35,24 +36,25 @@ module Pollios::V1::CurrentMemberAPI
         end
       end
 
-      desc "returns all requests related to current member"
+      desc 'returns all requests related to current member'
       resource :requests do
         params do
           optional :clear_new_request_count, type: Boolean, desc: "should clear member's new request count"
         end
         get do
-          requests_for_member =  Member::RequestList.new(current_member, {:clear_new_request_count => params[:clear_new_request_count]})
+          options = { clear_new_request_count: params[:clear_new_request_count] }
+          requests_for_member = Member::RequestList.new(current_member, options)
           present requests_for_member, with: RequestListEntity
         end
 
-        desc "return all requests to groups which current member is admin"
+        desc 'return all requests to groups which current member is admin'
         get '/group_admins' do
-          requests_for_member =  Member::RequestList.new(current_member)
+          requests_for_member = Member::RequestList.new(current_member)
           present requests_for_member, with: RequestListEntity, only: [:group_admins]
         end
       end
 
-      desc "returns notification and request counts for current member"
+      desc 'returns notification and request counts for current member'
       get '/badges' do
         { 
           notifications: current_member.notification_count, 
