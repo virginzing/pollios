bf524bb3d3dc01dca0f65a07dc8d572e9102da4b
Rawitat Pulam <rawitat@code-app.com>
2015-11-18 11:53:49 +0700

    further refactoring of guarding condition. broke down to 1-1 statement, then compose back

--- app/services/member/private/member_action_guard.rb
+++ app/services/member/private/member_action_guard.rb
@@ -2,26 +2,32 @@ module Member::Private::MemberActionGuard
 
   private
 
+  def same_member
+    member.id == a_member.id
+  end
+
+  def already_friend
+    member_list.already_friend_with?(a_member)
+  end
+
+  def already_sent_request
+    member_list.already_sent_request_to?(a_member)
+  end
+
+  def not_friend
+    member_list.not_friend_with?(a_member)
+  end
+
   def can_add_friend?
-    case 
-    when member.id == a_member.id
-      return false, "You can't add yourself as a friend."
-    when member_list.already_friend_with?(a_member)
-      return false, "You and #{a_member.get_name} are already friends."
-    when member_list.already_sent_request_to?(a_member)
-      return false, "You already sent friend request to #{a_member.get_name}"
-    else
-      return true, ''
-    end
+    return false, "You can't add yourself as a friend." if same_member
+    return false, "You and #{a_member.get_name} are already friends." if already_friend
+    return false, "You already sent friend request to #{a_member.get_name}" if already_sent_request
+    [true, '']
   end
 
   def can_unfriend?
-    case
-    when member_list.not_friend_with?(a_member)
-      return false, "You are not friends with #{friend.get_name}."
-    else
-      return true, ''
-    end      
+    return false, "You are not friends with #{friend.get_name}." if not_friend
+    [true, '']
   end
 
 end
