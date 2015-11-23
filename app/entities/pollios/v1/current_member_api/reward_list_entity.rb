1ea1ba3f888f64bbd2639005f2a7cee202794022
Rawitat Pulam <rawitat@code-app.com>
2015-11-23 11:38:33 +0700

    corrected v1 api for member's rewards

--- app/entities/pollios/v1/current_member_api/reward_list_entity.rb
+++ app/entities/pollios/v1/current_member_api/reward_list_entity.rb
@@ -1,6 +1,6 @@
 module Pollios::V1::CurrentMemberAPI
-	class RewardListEntity < Pollios::V1::BaseEntity
-		expose :rewards_at_current_page, as: :rewards, with: MemberRewardEntity
-		expose :next_page_index
-	end
+  class RewardListEntity < Pollios::V1::BaseEntity
+    expose :rewards_at_current_page, as: :rewards, with: MemberRewardEntity
+    expose :next_page_index
+  end
 end
