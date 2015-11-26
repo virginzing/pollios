5eb51373f21acfc613a08a3fe02cd0bc65b950f5
zingfox <zingfox@hotmail.com>
2015-11-26 11:34:43 +0700

    rollback friend relation helper

--- app/services/member/friend_relation_helper.rb
+++ app/services/member/friend_relation_helper.rb
@@ -6,7 +6,7 @@ module Member::FriendRelationHelper
   end
 
   private def friend_with?(a_member)
-    a_member.member_status == 1 && !blocked?(a_member)
+    a_member.member_status == 1
   end
 
   private def blocked?(a_member)
@@ -14,23 +14,23 @@ module Member::FriendRelationHelper
   end
 
   private def active_friend_with?(a_member)
-    active?(a_member) && !blocked?(a_member) && friend_with?(a_member)
+    active?(a_member) && friend_with?(a_member) && !blocked?(a_member)
   end
 
   private def requesting_friend_with?(a_member)
-    a_member.member_status == 0 && active?(a_member) && !blocked?(a_member)
+    a_member.member_status == 0 && active?(a_member)
   end
 
   private def following?(a_member)
-    a_member.member_following == true && !friend_with?(a_member) && !a_member.citizen? && !blocked?(a_member)
+    a_member.member_following == true && !friend_with?(a_member) && !a_member.citizen?
   end
 
   private def followed_by?(a_member)
-    a_member.member_following == true && !friend_with?(a_member) && !blocked?(a_member)
+    a_member.member_following == true && !friend_with?(a_member)
   end
 
   private def being_requested_friend_by?(a_member)
-    a_member.member_status == 2 && active?(a_member) && !blocked?(a_member)
+    a_member.member_status == 2 && active?(a_member)
   end
 
   private def blocked_friend?(a_member)
