83221e511fba4fe5de376ee12f22cc5632fe81b6
zingfox <zingfox@hotmail.com>
2015-12-02 14:40:56 +0700

    corrected group/member_list_entity visible

--- app/entities/pollios/v1/group_api/member_list_entity.rb
+++ app/entities/pollios/v1/group_api/member_list_entity.rb
@@ -7,7 +7,25 @@ module Pollios::V1::GroupAPI
     expose_members :pending
 
     def current_member_linkage
-      @current_member_linkage ||= Member::MemberList.new(options[:current_member]).social_linkage_ids
+      @current_member_linkage ||= Member::MemberList.new(current_member).social_linkage_ids
+    end
+
+    def requesting
+      return object.requesting if viewing_own_group?
+      return [current_member] if viewing_requesting_group?
+      []
+    end
+
+    def current_member
+      options[:current_member]  
+    end
+
+    def viewing_own_group?
+      object.admin?(current_member)
+    end
+
+    def viewing_requesting_group?
+      object.requesting?(current_member)
     end
   end
 end
