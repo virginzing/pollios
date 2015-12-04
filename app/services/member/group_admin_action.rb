3deb9dd51e0f5176d48a9dbb5df8231ac505ce6d
zingfox <zingfox@hotmail.com>
2015-12-04 15:40:34 +0700

    boilerplate admin:group_action

--- app/services/member/group_admin_action.rb
+++ app/services/member/group_admin_action.rb
@@ -13,18 +13,38 @@ class Member::GroupAdminAction < Member::GroupAction
   end
 
   def approve
+    can_approve, message = can_approve?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_approve
+      
+    process_approve
   end
 
   def deny
+    can_deny, message = can_deny?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_deny
+      
+    process_deny
   end
 
   def remove
+    can_remove, message = can_remove?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_remove
+      
+    process_remove
   end
 
   def promote
+    can_promote, message = can_promote?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_promote
+
+    process_promote
   end
 
   def demote
+    can_demote, message = can_demote?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_demote
+
+    process_demote
   end
 
   # refactor this into private module later
