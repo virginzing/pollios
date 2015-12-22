ef7abb18f8e71b5a5d0392854a498e85b04c76c1
Rawitat Pulam <rawitat@code-app.com>
2015-12-22 16:34:16 +0700

    fixed voting_info caching bug

--- app/services/member/poll_inquiry.rb
+++ app/services/member/poll_inquiry.rb
@@ -76,6 +76,6 @@ class Member::PollInquiry < Member::PollList
   end
 
   def cached_voting_detail
-    Rails.cache.fetch('member/#{member.id}/voting/#{poll.id}') { voting_detail }
+    Rails.cache.fetch("member/#{member.id}/voting/#{poll.id}") { voting_detail }
   end
 end
