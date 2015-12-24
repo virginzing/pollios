561486f2fc0325ce495d1962a6f5c318568148ea
zingfox <zingfox@hotmail.com>
2015-12-24 19:28:29 +0700

    moved method in member:poll_inquiry to member:private:poll_inquiry

--- app/services/member/poll_inquiry.rb
+++ app/services/member/poll_inquiry.rb
@@ -1,4 +1,5 @@
 class Member::PollInquiry < Member::PollList
+  include Member::Private::PollInquiry
 
   attr_reader :poll
 
@@ -29,54 +30,6 @@ class Member::PollInquiry < Member::PollList
     voting_allows, message = can_vote?    
     return voting_allows_hash if voting_allows
     voting_not_allowed_with_reason_hash(message)
-  end
-
-  def can_view?
-    return [false, ExceptionHandler::Message::Member::BAN] if poll.member.ban?
-    return [false, ExceptionHandler::Message::Poll::UNDER_INSPECTION] if poll.black?
-    return [false, ExceptionHandler::Message::Poll::CLOSED] if poll.closed?
-    return [false, ExceptionHandler::Message::Poll::EXPIRED] if poll.expire_date < Time.zone.now
-    return [false, ExceptionHandler::Message::Poll::DELETED] if poll.deleted_at.present?
-
-    [true, nil]
-  end
-
-  def can_vote?
-    return [false, ExceptionHandler::Message::Poll::OUTSIDE_GROUP] if member_outside_group_visibility?
-
-    [true, nil]
-  end
-
-  def member_outside_group_visibility?
-    need_group_visibility_check? && !group_ids_visible_to_member?
-  end
-
-  def need_group_visibility_check?
-    !poll.public && poll.in_group
-  end
-
-  def group_ids_visible_to_member?
-    in_group_ids = poll.in_group_ids.split(',').map(&:to_i)
-    member_active_group_ids = Member::GroupList.new(member).active_ids
-
-    visible_group_ids = in_group_ids & member_active_group_ids
-    !visible_group_ids.empty?
-  end
-
-  def voted_hash
-    { voted: true, choice_id: cached_voting_detail.first.choice_id }
-  end
-
-  def voting_allows_hash
-    { voted: false, can_vote: true }
-  end
-
-  def voting_not_allowed_with_reason_hash(message)
-    { voted: false, can_vote: false, reason: message }
-  end
-
-  def voting_detail
-    HistoryVote.member_voted_poll(member.id, poll.id).to_a
   end
 
   def cached_voting_detail
---
+++ app/services/member/private/poll_inquiry.rb
@@ -0,0 +1,53 @@
+module Member::Private::PollInquiry
+
+  private
+
+  def can_view?
+    return [false, ExceptionHandler::Message::Member::BAN] if poll.member.ban?
+    return [false, ExceptionHandler::Message::Poll::UNDER_INSPECTION] if poll.black?
+    return [false, ExceptionHandler::Message::Poll::CLOSED] if poll.closed?
+    return [false, ExceptionHandler::Message::Poll::EXPIRED] if poll.expire_date < Time.zone.now
+    return [false, ExceptionHandler::Message::Poll::DELETED] if poll.deleted_at.present?
+
+    [true, nil]
+  end
+
+  def can_vote?
+    return [false, ExceptionHandler::Message::Poll::OUTSIDE_GROUP] if member_outside_group_visibility?
+
+    [true, nil]
+  end
+
+  def member_outside_group_visibility?
+    need_group_visibility_check? && !group_ids_visible_to_member?
+  end
+
+  def need_group_visibility_check?
+    !poll.public && poll.in_group
+  end
+
+  def group_ids_visible_to_member?
+    in_group_ids = poll.in_group_ids.split(',').map(&:to_i)
+    member_active_group_ids = Member::GroupList.new(member).active_ids
+
+    visible_group_ids = in_group_ids & member_active_group_ids
+    !visible_group_ids.empty?
+  end
+
+  def voted_hash
+    { voted: true, choice_id: cached_voting_detail.first.choice_id }
+  end
+
+  def voting_allows_hash
+    { voted: false, can_vote: true }
+  end
+
+  def voting_not_allowed_with_reason_hash(message)
+    { voted: false, can_vote: false, reason: message }
+  end
+
+  def voting_detail
+    HistoryVote.member_voted_poll(member.id, poll.id).to_a
+  end
+  
+end
