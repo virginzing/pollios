2c5a9d113ff4dd6191c697dc253fb1224cc1b1f2
zingfox <zingfox@hotmail.com>
2015-12-28 21:04:19 +0700

    added member:poll_action guard

--- app/services/member/poll_action.rb
+++ app/services/member/poll_action.rb
@@ -12,36 +12,6 @@ class Member::PollAction
     create_poll_viewing_record
   end
 
-  private
-
-  def create_poll_viewing_record
-    return if HistoryView.exists?(member_id: member.id, poll_id: poll.id)
-    
-    create_history_view_for_member
-    FlushCached::Member.new(member).clear_list_history_viewed_polls
-  end
-
-  def create_history_view_for_member
-    HistoryView.transaction do
-      HistoryView.create! member_id: member.id, poll_id: poll.id
-      create_company_group_action_tracking_record_for_action('view')
-
-      poll.with_lock do
-        poll.view_all += 1
-        poll.save!
-      end
-    end
-  end
-
-  def create_company_group_action_tracking_record_for_action(action)
-    return unless poll.in_group
-
-    group_ids = poll.in_group_ids.split(',').map(&:to_i)
-    group_ids.each do |group_id|
-      member.activity_feeds.create! action: action, trackable: poll, group_id: group_id
-    end
-  end
-
   def create
     can_create, message = can_create?
     fail ExceptionHandler::UnprocessableEntity, message unless can_create
---
+++ app/services/member/private/poll_action_guard.rb
@@ -0,0 +1,54 @@
+module Member::Private::PollActionGuard
+
+  private
+
+  def can_create?
+    [true, nil]
+  end
+
+  def can_close?
+    [true, nil]
+  end
+
+  def can_vote?
+    [true, nil]
+  end
+
+  def can_bookmark?
+    [true, nil]
+  end
+
+  def can_unbookmark?
+    [true, nil]
+  end
+
+  def can_save?
+    [true, nil]
+  end
+
+  def can_watch?
+    [true, nil]
+  end
+
+  def can_unwatch?
+    [true, nil]
+  end
+
+  def can_not_interest?
+    [true, nil]
+  end
+
+  def can_promote?
+    [true, nil]
+  end
+
+  def can_report?
+
+    [true, nil]
+  end
+
+  def can_comment?
+    [true, nil]
+  end
+
+end
