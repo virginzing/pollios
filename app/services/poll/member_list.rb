77fd16a9a1200fab2513a35282307d2381634992
zingfox <zingfox@hotmail.com>
2016-01-12 15:47:07 +0700

    added guard for poll:member_list

--- app/services/poll/member_list.rb
+++ app/services/poll/member_list.rb
@@ -7,6 +7,17 @@ class Poll::MemberList
 
     return unless options[:viewing_member]
     @viewing_member = options[:viewing_member]
+
+    can_view, message = can_view?
+    fail ExceptionHandler::UnprocessableEntity, message unless can_view
+  end
+
+  def can_view?
+    return [true, nil] unless viewing_member
+    can_view, message = poll_inquiry_service.can_view?
+    return [false, message] unless can_view
+
+    [true, nil]
   end
 
   def voter
@@ -33,5 +44,9 @@ class Poll::MemberList
   def voter_visibility
     return all_voter unless viewing_member
     all_voter.viewing_by_member(viewing_member)
+  end
+
+  def poll_inquiry_service
+    Member::PollInquiry.new(viewing_member, poll)
   end
 end
