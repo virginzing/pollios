43e1f28caacd2f96d60dfac451f47d4b4d4111fb
zingfox <zingfox@hotmail.com>
2016-02-17 16:32:04 +0700

    fixed correcct random_from_voter in campaign:reward_prediction

--- app/services/campaign/reward_prediction.rb
+++ app/services/campaign/reward_prediction.rb
@@ -22,18 +22,18 @@ class Campaign::RewardPrediction
   end
 
   def random_reward_later
-    create_member_reward(:waiting_announce, member.id, ref_no: generate_ref_no)
+    create_member_reward(:waiting_announce, member.id)
   end
 
   def random_reward
     reward_id = random_reward_id
     if reward_id == 0
-      create_member_reward(:not_receive, member.id, ref_no: generate_ref_no)
+      create_member_reward(:not_receive, member.id)
       send_not_recive_reward_notification(member_id.to_a)
     else
       increment_used_campaign_claimed_reward(reward_id)
       member_reward = create_member_reward(:receive, member.id, reward_id: reward_id \
-        , serial_code: generate_serial_code, ref_no: generate_ref_no)
+        , serial_code: generate_serial_code)
       send_recive_reward_notification(member_reward.id)
     end
   end
@@ -61,7 +61,7 @@ class Campaign::RewardPrediction
   def create_member_reward(reward_status, member_id, options = {})
     campaign.member_rewards.create!(reward_status: reward_status, poll_id: poll.id \
       , member_id: member_id, reward_id: options[:reward_id] \
-      , serial_code: options[:serial_code], ref_no: options[:ref_no])
+      , serial_code: options[:serial_code], ref_no: generate_ref_no)
   end
 
   def random_from_voter
@@ -71,8 +71,8 @@ class Campaign::RewardPrediction
       member_get_reward_ids = voter_ids.sample(reward.total)
 
       member_get_reward_ids.each do |member_id|
-        member_reward = create_member_reward(:receive, member_id, reward_id: reward.id \
-        , serial_code: generate_serial_code, ref_no: generate_ref_no)
+        member_reward = campaign.member_rewards.find_by(member_id: member_id).update!(reward_status: :receive \
+                          , reward_id: reward.id, serial_code: generate_serial_code)
         increment_used_campaign_claimed_reward(reward.id)
         send_recive_reward_notification(member_reward.id)
       end
