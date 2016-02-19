b6cc5248b3dbdfadbc915bc1390e8c43397ae0a0
zingfox <zingfox@hotmail.com>
2016-02-19 12:42:09 +0700

    encode poll_id for share on facebook

--- app/services/generate_qrcode_link.rb
+++ app/services/generate_qrcode_link.rb
@@ -3,9 +3,10 @@ class GenerateQrcodeLink
   def initialize(poll_or_questionnaire)
     @poll_or_questionnaire = poll_or_questionnaire
     @series = false
-    @host_link = Rails.env.production? ? 'http://pollios.com' : 'http://192.168.1.101:3000'
+    @host_link = Rails.env.production? ? 'http://pollios.com' : 'http://192.168.1.17:3000'
     @custom_url = '/m/polls?key='
     @redirect_url = '/qrcode?key='
+    @open_app = 'pollios://poll?poll_id='
   end
 
   def get_link
@@ -14,6 +15,10 @@ class GenerateQrcodeLink
 
   def get_redirect_link
     @host_link + @redirect_url + secret_qrcode_key
+  end
+
+  def link
+    @open_app + secret_qrcode_key
   end
 
   private
