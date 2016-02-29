6ef1e166cd72a614e0c6d592757370ff2f436dc7
zingfox <zingfox@hotmail.com>
2016-02-29 15:48:20 +0700

    beautify service v1 Member::RequestList

--- app/entities/pollios/v1/shared/group_for_admin_list_entity.rb
+++ app/entities/pollios/v1/shared/group_for_admin_list_entity.rb
@@ -1,5 +1,5 @@
 module Pollios::V1::Shared
   class GroupForAdminListEntity < Pollios::V1::Shared::GroupForListEntity
-    expose :members_request, as: :request, with: MemberEntity
+    expose :members_request, as: :request, with: MemberForListEntity
   end
 end
--- app/services/member/request_list.rb
+++ app/services/member/request_list.rb
@@ -9,23 +9,23 @@ class Member::RequestList
   end
 
   def friends_incoming
-    member_list.friend_request
+    member_listing.friend_request
   end
 
   def friends_outgoing
-    member_list.your_request
+    member_listing.your_request
   end
 
   def group_invitations
-    group_list.got_invitations
+    group_listing.got_invitations
   end
 
   def group_requests
-    group_list.requesting_to_joins
+    group_listing.requesting_to_joins
   end
 
   def group_admins
-    group_list.as_admin_with_requests
+    group_listing.as_admin_with_requests
   end
 
   def recommended_friends
@@ -46,20 +46,16 @@ class Member::RequestList
 
   private
 
-  def member_list
-    @member_list ||= Member::MemberList.new(member)
+  def member_listing
+    @member_listing ||= Member::MemberList.new(member)
   end
 
-  def group_list
-    @group_list ||= Member::GroupList.new(member)
+  def group_listing
+    @group_listing ||= Member::GroupList.new(member)
   end
 
   def recommendations
     @recommendations ||= Member::Recommendation.new(member)
-  end
-
-  def current_member_linkage
-    @current_member_linkage ||= Member::MemberList.new(options[:current_member]).social_linkage_ids
   end
 
   def reset_new_request_count    
