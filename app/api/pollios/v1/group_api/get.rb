516c3b5683e55a9f4001a5c858719301df3c47b9
zingfox <zingfox@hotmail.com>
2016-02-29 19:02:56 +0700

    added current_member_status, current_member_linkage, current_member_linkage in get group_api

--- app/api/pollios/v1/group_api/get.rb
+++ app/api/pollios/v1/group_api/get.rb
@@ -6,6 +6,10 @@ module Pollios::V1::GroupAPI
       def group
         @group ||= Group.cached_find(params[:id])
       end
+
+      def current_member_status
+        @current_member_status ||= Member::GroupList.new(current_member).relation_status_ids
+      end
     end
 
     resource :groups do
@@ -17,18 +21,22 @@ module Pollios::V1::GroupAPI
         
         desc 'returns group details for group_id'
         get do
-          present group, with: Pollios::V1::Shared::GroupEntity, current_member: current_member
+          present group, with: Pollios::V1::Shared::GroupEntity \
+            , current_member_status: current_member_status
         end
 
         desc 'returns group details with lastest join member for group_id'
         get '/recents' do
-          present group, with: Pollios::V1::Shared::GroupWithLastestMemberEntity, current_member: current_member
+          present group, with: Pollios::V1::Shared::GroupWithLastestMemberEntity \
+            , current_member_status: current_member_status
         end
 
         desc 'returns members of group_id'
         get '/members' do
           members_of_group = Group::MemberList.new(group, viewing_member: current_member)
-          present members_of_group, with: MemberListEntity, current_member: current_member
+          present members_of_group, with: MemberListEntity \
+            , current_member: current_member \
+            , current_member_linkage: Member::MemberList.new(current_member).social_linkage_ids
         end
 
         desc 'returns polls in group_id'
@@ -37,7 +45,9 @@ module Pollios::V1::GroupAPI
         end
         get '/polls' do
           polls_of_groups = Group::PollList.new(group, viewing_member: current_member, index: params[:index])
-          present polls_of_groups, poll: :polls, with: Pollios::V1::Shared::PollListEntity, current_member: current_member
+          present polls_of_groups, poll: :polls, with: Pollios::V1::Shared::PollListEntity \
+            , current_member: current_member \
+            , current_member_states: Member::PollList.new(current_member).member_states_ids
         end
 
         desc "returns group's settings details"
