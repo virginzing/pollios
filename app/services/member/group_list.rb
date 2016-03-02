fabbb076f81e84dea54bb0cc4930c7fa430f9ed0
zingfox <zingfox@hotmail.com>
2016-03-02 15:06:14 +0700

    fixed service member:group_list

--- app/services/member/group_list.rb
+++ app/services/member/group_list.rb
@@ -102,7 +102,7 @@ class Member::GroupList
   end
 
   def hash_member_count
-    @group_member_count ||= group_member_count.inject({}) { |hash, group| hash[group.id] = group.member_count; hash }
+    @group_member_count ||= group_member_count.each_with_object({}) { |group, hash| hash[group.id] = group.member_count }
   end
 
   private
@@ -125,13 +125,20 @@ class Member::GroupList
 
 
   def groups
-    Group.joins(:group_members).without_deleted.select("groups.*, group_members.is_master as member_admin, group_members.active as member_is_active, group_members.invite_id as member_invite_id") \
-      .where("group_members.member_id = #{@member.id}").group("groups.id, member_admin, member_is_active, member_invite_id")
+    Group.joins(:group_members).without_deleted
+      .select(
+        "groups.*, group_members.is_master AS member_admin,
+        group_members.active AS member_is_active,
+        group_members.invite_id AS member_invite_id")
+      .where("group_members.member_id = #{@member.id}")
+      .group('groups.id, member_admin, member_is_active, member_invite_id')
   end
 
   def group_member_count
-    Group.joins(:group_members_active).without_deleted.select("groups.*, count(group_members) as member_count").group("groups.id") \
-      .where("groups.id IN (?)", cached_all_groups.map(&:id))
+    Group.joins(:group_members_active).without_deleted
+      .select('groups.*, count(group_members) AS member_count')
+      .group('groups.id')
+      .where('groups.id IN (?)', cached_all_groups.map(&:id))
   end
 
   def cached_groups
