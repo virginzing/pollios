2557cd1bc82bfab668794e6a66ba03e121a5f3ba
zingfox <zingfox@hotmail.com>
2016-03-07 18:29:04 +0700

    moved private method in member:group_list to member:private:group_list

--- app/services/member/group_list.rb
+++ app/services/member/group_list.rb
@@ -1,4 +1,5 @@
 class Member::GroupList
+  include Member::Private::GroupList
 
   attr_reader :member, :viewing_member
   
@@ -105,57 +106,4 @@ class Member::GroupList
     @group_member_count ||= group_member_count.each_with_object({}) { |group, hash| hash[group.id] = group.member_count }
   end
 
-  private
-
-  def ids_for(list)
-    list.map(&:id)
-  end
-
-  def viewing_own_profile
-    viewing_member.id == member.id
-  end
-
-  def viewing_member_visibility_for_group(group)
-    group.public? || Member::GroupList.new(viewing_member).member_of?(group)
-  end
-
-  def groups_for_viewing_member
-    cached_groups.select { |group| group if viewing_member_visibility_for_group(group) }
-  end
-
-
-  def groups
-    groups = Group.joins(:group_members).without_deleted
-             .select(
-               "groups.*, group_members.is_master AS member_admin,
-               group_members.active AS member_is_active,
-               group_members.invite_id AS member_invite_id")
-             .where("group_members.member_id = #{member.id}")
-             .group('groups.id, member_admin, member_is_active, member_invite_id')
-
-    groups.each do |group|
-      group.member_invite_id = nil if group.member_invite_id == member.id
-    end
-
-    groups
-  end
-
-  def group_member_count
-    Group.joins(:group_members_active).without_deleted
-      .select('groups.*, count(group_members) AS member_count')
-      .group('groups.id')
-      .where('groups.id IN (?)', cached_all_groups.map(&:id))
-  end
-
-  def cached_groups
-    Rails.cache.fetch("member/#{member.id}/groups") do
-      groups.to_a
-    end
-  end
-
-  # def cached_groups_for_poll(poll)
-  #   Rails.cache.fetch("poll/#{poll.id}/groups") do
-  #     poll.groups
-  #   end
-  # end
 end
