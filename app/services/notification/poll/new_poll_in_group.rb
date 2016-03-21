83481e1f75f551da467216dbc253a4e1725af2c9
zingfox <zingfox@hotmail.com>
2016-03-21 23:00:32 +0700

    added worker & service new poll in group

---
+++ app/services/notification/poll/new_poll_in_group.rb
@@ -0,0 +1,45 @@
+class Notification::Poll::NewPollInGroup
+  include Notification::Helper
+  include SymbolHash
+
+  attr_reader :member, :poll, :group
+
+  def initialize(member, poll, group)
+    @member = member
+    @poll = poll
+    @group = group
+
+    create_notification(member, recipient_list, type, message, data)
+  end
+
+  def type
+    'group'
+  end
+
+  def recipient_list
+    group_member_listing_service = Group::MemberList.new(group)
+    recipient_list = group_member_listing_service.active
+
+    member_listing_service = Member::MemberList.new(member)
+    blocked_members = member_listing_service.blocks | Member.find(member_listing_service.blocked_by_someone)
+    recipient_list - blocked_members - [member]
+  end
+
+  def message
+    member.fullname + " asked in #{group.name}: \"#{@poll.title}\""
+  end
+
+  def data
+    @data ||= {
+      type: TYPE[:poll],
+      poll_id: poll.id,
+      poll: PollSerializer.new(poll).as_json,
+      series: poll.series,
+      action: ACTION[:create],
+      worker: WORKER[:poll],
+      group_id: group.id,
+      group: GroupNotifySerializer.new(group).as_json
+    }
+  end
+
+end
---
+++ app/workers/v1/new_poll_in_group_worker.rb
@@ -0,0 +1,12 @@
+class V1::NewPollInGroupWorker
+  include Sidekiq::Worker
+  sidekiq_options unique: true
+
+  def perform(member_id, poll_id, group_id)
+    member = Member.cached_find(member_id)
+    poll = Poll.cached_find(poll_id)
+    group = Group.cached_find(group_id)
+
+    Notification::Poll::NewPollInGroup.new(member, poll, group)
+  end
+end
