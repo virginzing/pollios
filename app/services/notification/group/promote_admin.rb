3621de16cbaa0c8efc009775a8a5e7f5abe572ce
zingfox <zingfox@hotmail.com>
2016-03-29 15:54:52 +0700

    added service & worker notification:group:promote_admin

---
+++ app/services/notification/group/promote_admin.rb
@@ -0,0 +1,34 @@
+class Notification::Group::PromoteAdmin
+  include Notification::Helper
+  include SymbolHash
+
+  def initialize(member, a_member, group)
+    @member = member
+    @a_member = a_member
+    @group = group
+
+    create_notification(recipient_list, type, message, data)
+  end
+
+  def type
+    'request'
+  end
+
+  def recipient_list
+    [a_member]
+  end
+
+  def message
+    member.fullname + " promoted you to administrator of #{@group.name} group"
+  end
+
+  def data
+    @data ||= {
+      type: TYPE[:group],
+      group_id: group.id,
+      group: GroupNotifySerializer.new(group).as_json,
+      action: ACTION[:promote_admin],
+      worker: WORKER[:promote_admin]
+    }
+  end
+end
