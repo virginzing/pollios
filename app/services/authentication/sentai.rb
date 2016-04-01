3719f1ca2dee670f4b48576af8845ff3744118ed
zingfox <zingfox@hotmail.com>
2016-04-01 10:48:52 +0700

    fixed symbol in development server

--- app/services/authentication/sentai.rb
+++ app/services/authentication/sentai.rb
@@ -5,7 +5,7 @@ class Authentication::Sentai
     development_sentai_url = 'http://codeapp-user-dev.herokuapp.com'
 
     if Rails.env.production?
-      return development_sentai_url if ENV[:DEVELOPMENT_SERVER].to_b
+      return development_sentai_url if ENV['DEVELOPMENT_SERVER'].to_b
       production_sentai_url
     else
       development_sentai_url
