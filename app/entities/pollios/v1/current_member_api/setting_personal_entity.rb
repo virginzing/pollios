86376b9435efd1f00d245e2ac235d4ae36745e60
zingfox <zingfox@icloud.com>
2016-04-26 11:14:06 +0700

    added form in api current_member/settings/personal

--- app/entities/pollios/v1/current_member_api/setting_personal_entity.rb
+++ app/entities/pollios/v1/current_member_api/setting_personal_entity.rb
@@ -1,8 +1,32 @@
 module Pollios::V1::CurrentMemberAPI
   class SettingPersonalEntity < Pollios::V1::BaseEntity
 
+    expose :form
     expose :birthday
     expose :gender
+
+    def form
+      [
+        {
+          title: 'Basic Info',
+          fields: [
+            {
+              type: 'date',
+              key: 'birthday',
+              title: 'Birthday',
+              placeholder: "What's you birthday?"
+            },
+            {
+              type: 'single-choice',
+              key: 'gender',
+              title: 'Gender',
+              placeholder: 'Please select your gender',
+              choices: Member.gender.values.collect { |e| { title: e.text, value: e.value } }
+            }
+          ]
+        }
+      ]
+    end
 
   end
 end
