29f4cef460b5bf0b2389c321ab411560ecb4165b
zingfox <zingfox@icloud.com>
2016-06-15 00:24:48 +0700

    fixed admin:notification push to using Rpush

--- app/services/notify/push_notification.rb
+++ app/services/notify/push_notification.rb
@@ -1,47 +1,79 @@
 class Notify::PushNotification
 
+  attr_reader :params
+
   def initialize(params = {})
     @params = params
   end
 
-  def env
-    if @params[:env] == "production"
-      APNS.host = "gateway.push.apple.com"
-      APNS.pem = File.join(Rails.root, 'config', 'certificates', 'server_production', 'PolliosProduction.pem')
+  # def env
+  #   if @params[:env] == "production"
+  #     APNS.host = "gateway.push.apple.com"
+  #     APNS.pem = File.join(Rails.root, 'config', 'certificates', 'server_production', 'PolliosProduction.pem')
+  #   else
+  #     APNS.host = "gateway.sandbox.push.apple.com"
+  #     APNS.pem = File.join(Rails.root, 'config', 'certificates', 'local', 'PolliosDev.pem')
+  #   end
+  # end
+
+  # def device_token
+  #   @params[:device_token]
+  # end
+
+  # def alert
+  #   @params[:alert].presence || 'Pollios'
+  # end
+
+  # def badge
+  #   @params[:badge].to_i == 0 ? 1 : @params[:badge].to_i
+  # end
+
+  # def sound
+  #   @params[:sound].presence || 'default'
+  # end
+
+  # def new_notification
+  #   APNS::Notification.new(device_token, alert: alert, badge: badge, sound: sound, content_available: 1)
+  # end
+
+  # def send
+  #   env
+  #   n = new_notification
+  #   APNS.send_notifications([n])
+  #   true
+  # end
+
+  def send
+    notification = Rpush::Apns::Notification.new
+    notification.app = app
+    notification.device_token = params[:device_token]
+    notification.alert = alert
+    notification.badge = badge
+    notification.sound = sound
+    notification.save!
+  end
+
+  private
+
+  def app
+    if params[:env] == 'production'
+      Rpush::Apns::App.first
     else
-      APNS.host = "gateway.sandbox.push.apple.com"
-      APNS.pem = File.join(Rails.root, 'config', 'certificates', 'local', 'PolliosDev.pem')
+      # Beta app
     end
   end
 
-  def device_token
-    @params[:device_token]
-  end
-
   def alert
-    @params[:alert].presence || 'Pollios'
+    params[:alert].presence || 'Pollios'
   end
 
   def badge
-    @params[:badge].to_i == 0 ? 1 : @params[:badge].to_i
+    params[:badge].to_i == 0 ? 1 : params[:badge].to_i
   end
 
   def sound
-    @params[:sound].presence || 'default'
+    params[:sound].presence || true
   end
-
-  def new_notification
-    APNS::Notification.new(device_token, alert: alert, badge: badge, sound: sound, content_available: 1)
-  end
-
-  def send
-    env
-    n = new_notification
-    APNS.send_notifications([n])
-    true
-  end
-
-
 
 end
 
