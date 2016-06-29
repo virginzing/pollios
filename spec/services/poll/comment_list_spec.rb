0da0d007d14fab2430d7fd9f8ff8fafe9d5dbb96
Nanthipath <omliler_man@hotmail.com>
2016-06-29 21:02:33 +0700

    add it voter viewing by commets

--- spec/services/poll/comment_list_spec.rb
+++ spec/services/poll/comment_list_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 pathname = Pathname.new(__FILE__)
-RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n Poll::CommentList"
+RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n Poll::CommentList" do
 
+  let(:new_poll) { FactoryGirl.create(:poll) }
+
+  let(:new_member1) { FactoryGirl.create(:member) }
+  let(:new_member2) { FactoryGirl.create(:member) }
+  let(:new_member3) { FactoryGirl.create(:member) }
+
+  let(:new_member1_poll_action) { Member::PollAction.new(new_member1, new_poll) }  
+  let(:new_member2_poll_action) { Member::PollAction.new(new_member2, new_poll) }
+  let(:new_member3_poll_action) { Member::PollAction.new(new_member3, new_poll) }
+
+  let(:new_member3_poll_comment_list) { Poll::CommentList.new(new_poll, viewing_member: new_member3) }
+  
+  context 'Comment' do
+    
+    it '- Voter viewing by comments' do
+      new_member1_poll_action.vote(choice_id: new_poll.choices.first.id)
+      new_member2_poll_action.vote(choice_id: new_poll.choices.second.id)
+      new_member3_poll_action.vote(choice_id: new_poll.choices.second.id)
+
+      new_member1_poll_action.comment(message: 'test comment member1')
+      new_member2_poll_action.comment(message: 'test comment member2')
+
+      expect(new_member3_poll_comment_list.comments.map(&:id)).to \
+        match_array(new_poll.comments.map(&:id))
+    end
+  end
 end
