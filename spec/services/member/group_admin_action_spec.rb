67508eabaf66de84db9de918cfcd6d1291291d7d
Chinthith Wisetsombat <chinnonae@gmail.com>
2016-06-29 11:33:15 +0700

    add spec for Member::GroupAdminAction.deny

--- spec/services/member/group_admin_action_spec.rb
+++ spec/services/member/group_admin_action_spec.rb
@@ -33,4 +33,30 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
   end
 
+  context '#deny: A need-approve-group admin deny a join request' do
+    before(:context) {
+      @need_approve_group = Member::GroupAction.new(@group_admin).create(FactoryGirl.attributes_for(:group_that_need_approve))
+      @admin_group_action = Member::GroupAction.new(@group_admin, @need_approve_group)
+      @one_group_action = Member::GroupAction.new(@member, @need_approve_group)
+      @group_admin_action_on_one = Member::GroupAdminAction.new(@group_admin, @need_approve_group, @member)
+    }
+
+    it 'A group admin deny a join request' do
+      @one_group_action.join
+      @group_admin_action_on_one.deny
+      expect(Group::MemberList.new(@need_approve_group).members.size).to eq(0)
+      expect(Group::MemberList.new(@need_approve_group).requesting.size).to eq(0)
+    end
+
+    it 'A group admin tries to deny a join request but the member is already in the group' do
+      @one_group_action.join
+      @group_admin_action_on_one.approve
+      expect{ @group_admin_action_on_one.deny }.to raise_error(ExceptionHandler::UnprocessableEntity, "#{@member.get_name} is already in #{@need_approve_group.name}.")
+    end
+
+    it 'A group admin tries to deny a request that has never been sent to admin.' do
+      expect{ @group_admin_action_on_one.deny }.to raise_error(ExceptionHandler::UnprocessableEntity, "#{@member.get_name} doesn't have any request to #{@need_approve_group.name}.")
+    end
+  end
+
 end
