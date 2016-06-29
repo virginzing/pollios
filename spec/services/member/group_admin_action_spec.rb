91099da9cfdae60dc6e607f9d538b0c836692915
Chinthith Wisetsombat <chinnonae@gmail.com>
2016-06-29 11:30:37 +0700

    change let(s) to before(:context) in spec Member::GroupAdminAction0

--- spec/services/member/group_admin_action_spec.rb
+++ spec/services/member/group_admin_action_spec.rb
@@ -2,19 +2,34 @@ require 'rails_helper'
 
 pathname = Pathname.new(__FILE__)
 RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n Member::GroupAdminAction" do
-  let!(:group_admin) { FactoryGirl.create(:member, email: Faker::Internet.email) }
-  let!(:member_one) { FactoryGirl. create(:member, email: Faker::Internet.email, fullname: 'Member One') }
 
-  context '#approve: A group admin approve a join-request' do
-    let(:need_approve_group) { Member::GroupAction.new(group_admin).create(FactoryGirl.attributes_for(:group_that_need_approve)) }
-    let(:admin_group_action) { Member::GroupAction.new(group_admin, need_approve_group) }
-    let(:one_group_action) { Member::GroupAction.new(member_one, need_approve_group) }
-    let(:group_admin_action_on_one) { Member::GroupAdminAction.new(group_admin, need_approve_group, member_one) }
+  before(:context) {
+    @group_admin = FactoryGirl.create(:member, email: Faker::Internet.email)
+    @member = FactoryGirl.create(:member, fullname: 'Member One', email: Faker::Internet.email)
+  }
 
-    it 'A member request to join the group and admin approve' do
-      one_group_action.join
-      group_admin_action_on_one.approve
-      expect(Group::MemberList.new(need_approve_group).members.map(&:id)).to match_array [member_one.id]
+  context '#approve: A need-approve-group admin approves a join request' do
+    before(:context) {
+      @need_approve_group = Member::GroupAction.new(@group_admin).create(FactoryGirl.attributes_for(:group_that_need_approve))
+      @admin_group_action = Member::GroupAction.new(@group_admin, @need_approve_group)
+      @one_group_action = Member::GroupAction.new(@member, @need_approve_group)
+      @group_admin_action_on_one = Member::GroupAdminAction.new(@group_admin, @need_approve_group, @member)
+    }
+
+    it 'A group admin approve a join request' do
+      @one_group_action.join
+      @group_admin_action_on_one.approve
+      expect(Group::MemberList.new(@need_approve_group).members.map(&:id)).to match_array [@member.id]
+    end
+
+    it 'A group admin tries to approve a join request but the member is already in the group.' do
+      @one_group_action.join
+      @group_admin_action_on_one.approve
+      expect{ @group_admin_action_on_one.approve }.to raise_error(ExceptionHandler::UnprocessableEntity, "#{@member.get_name} is already in #{@need_approve_group.name}.")
+    end
+
+    it 'A group admin tries to approve a request that has never been sent to admin.' do
+      expect{ @group_admin_action_on_one.approve }.to raise_error(ExceptionHandler::UnprocessableEntity, "#{@member.get_name} doesn't sent join request to #{@need_approve_group.name}.")
     end
   end
 
