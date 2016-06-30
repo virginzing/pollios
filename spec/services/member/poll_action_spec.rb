495cb649f6bb88826e80e6b8b90b95c7ecf85d9b
Wasut <wasut_pao@hotmail.com>
2016-06-30 18:59:26 +0700

    add spec for Member::PollAction.delete

--- spec/services/member/poll_action_spec.rb
+++ spec/services/member/poll_action_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
   end
 
   context '#create: A member create poll, became owner of the poll' do
-
     before(:context) do
       @poll = Member::PollAction.new(@poll_creator).create(FactoryGirl.attributes_for(:poll, :with_choices))
     end
@@ -22,10 +21,30 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
       expect(@poll_creator.id).to eq(@poll.member_id)
     end
 
-    it '- A Poll default is not public' do
+    it '- A poll default is not public' do
       expect(@poll.public).to be false
     end
     
+  end
+
+  context '#delete: A member delete poll' do
+    before(:context) do
+      @poll = Member::PollAction.new(@poll_creator).create(FactoryGirl.attributes_for(:poll, :with_choices))
+      @member_poll_action = Member::PollAction.new(@member, @poll)
+      @creator_poll_action = Member::PollAction.new(@poll_creator, @poll)
+    end
+
+    it '- A member who is not owner this poll could not delete this poll' do
+      expect{ @member_poll_action.delete }.to raise_error(ExceptionHandler::UnprocessableEntity, "You're not owner this poll.")
+    end
+
+    it '- A member who is owner this poll could delete this poll' do
+      expect{ @creator_poll_action.delete }.not_to raise_error
+    end
+
+    it '- A poll that delete must have value of deleted_at' do
+      expect(@poll.deleted_at).not_to eq(nil)
+    end
   end
 
 end
