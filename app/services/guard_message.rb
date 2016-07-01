5ba90a544d69df407dbd1f5a20db301788044194
frfahrung <frfahrung95@gmail.com>
2016-07-01 16:09:04 +0700

    add guard message in app/services/guard_message

--- app/services/guard_message.rb
+++ app/services/guard_message.rb
@@ -22,3 +22,87 @@
 #   'ปริมาณสินค้าที่จะเพิ่มไม่เพียงพอ'
 # end
 ##########################
+
+def already_friend_message(a_member)
+  "You and #{a_member.get_name} are already friends."
+end
+
+def already_sent_request_message(a_member)
+  "You already sent friend request to #{a_member.get_name}."
+end
+
+def already_block_message(a_member)
+  "You are currently blocking #{a_member.get_name}."
+end
+
+def already_follow_message
+  "You already followed this account."
+end
+
+def already_block_message
+  "You already blocked #{a_member.get_name}."
+end
+
+def add_themself_as_a_friend_message
+  "You can't add yourself as a friend."
+end
+
+def unfriend_themself_message
+  "You can't unfriend yourself."
+end
+
+def follow_themself_message
+  "You can't follow yourself."
+end
+
+def unfollow_themself_message
+  "You can't unfollow yourself."
+end
+
+def block_themself_message
+  "You can't block yourself."
+end
+
+def unblock_themself_message
+  "You can't unblock yourself."
+end
+
+def report_themself_message
+  "You can't report yourself."
+end
+
+def not_friend_message(a_member)
+  "You are not friends with #{a_member.get_name}."
+end
+
+def not_official_account_message
+  "This member is not official account."
+end
+
+def not_following_message
+  "You are not following this account."
+end
+
+def not_blocking_message(a_member)
+  "You are not blocking #{a_member.get_name}."
+end
+
+def not_exist_incoming_request_message(a_member)
+  "You don't have friend request from #{a_member.get_name}"
+end
+
+def friends_limit_exceed_message(a_member)
+  "#{a_member.get_name} has over #{a_member.friend_limit} friends."
+end
+
+def accept_incoming_block_message
+  "You can't accept this request at this moment."
+end
+
+def not_exist_incoming_request_message
+  "This request is not existing."
+end
+
+def not_exist_outgoing_request_message
+  "This request is not existing."
+end
