064a722b4caf0510a1f892182eab3c74ed3f9a94
CatFly Chittawan <c.catfly@gmail.com>
2016-07-01 14:35:02 +0700

    Add app/services/guard_message.rb and a RSpec file example

---
+++ app/services/guard_message.rb
@@ -0,0 +1,24 @@
+# These methods are examples. Please kindly place new error message below these comments
+
+# *** Note : please add /require 'guard_message'/ with no backslashes to the top of every action guard file.
+
+# def address_not_exist_message
+#   'คุณไม่มีที่อยู่นี้'
+# end
+
+# def address_name_already_exists_message(name)
+#   "คุณมีที่อยู่ชื่อ #{name} แล้ว"
+# end
+
+# def invoice_not_exist_message
+#   'คุณไม่มียอดค้างชำระสินค้านี้อยู่'
+# end
+
+# def product_already_favourited_message
+#   'สินค้านี้โดนใจคุณไปแล้ว'
+# end
+
+# def insufficient_stock_message
+#   'ปริมาณสินค้าที่จะเพิ่มไม่เพียงพอ'
+# end
+##########################
