2957c55c3bb19857865b2c8eea07e65256a038ba
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-07-04 16:14:44 +0700

    remove Action calls and fix typo in spec Member::GroupList

--- spec/services/member/group_list_spec.rb
+++ spec/services/member/group_list_spec.rb
@@ -10,31 +10,29 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     @outsider = FactoryGirl.create(:member)
   end
 
-  context '#member_of?: Is a member is in a group(not public) in insider(who is in the group) point of view.' do
+  context '#member_of?: Is a member in a group(not public) in insider(who is in the group) point of view.' do
     it '- When the member is ordinary member, the insider should see that the member is in the group.' do
       expect(Member::GroupList.new(@group_members[0], viewing_member: @group_members[3]).member_of?(@group)).to be true
     end
 
     it '- When the member is admin, the insider should see that the member is in the group.' do
-      Member::GroupAdminAction.new(@group_admin, @group, @group_members[1]).promote
-      expect(@group.group_members.detect { |m| m.member_id = @group_members[1].id }.is_master).to be true
+      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
       expect(Member::GroupList.new(@group_members[1], viewing_member: @group_members[3]).member_of?(@group)).to be true
     end
   end
 
-  context '#member_of?: Is a member is in a group(not public) in outsider(who is not in the group) point of view.' do
+  context '#member_of?: Is a member in a group(not public) in outsider(who is not in the group) point of view.' do
     it '- When the member is ordinary member, the outsider should see that the member is in the group.' do
       expect(Member::GroupList.new(@group_members[0], viewing_member: @outsider).member_of?(@group)).to be false
     end
 
     it '- When the member is admin, the outsider should see that the member is in the group.' do
-      Member::GroupAdminAction.new(@group_admin, @group, @group_members[1]).promote
-      expect(@group.group_members.detect { |m| m.member_id = @group_members[1].id }.is_master).to be true
+      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
       expect(Member::GroupList.new(@group_members[1], viewing_member: @outsider).member_of?(@group)).to be false
     end
   end
 
-  context '#member_of?: Is a member is an a public group in outsider(who is not in the group) point of view.' do
+  context '#member_of?: Is a member a public group in outsider(who is not in the group) point of view.' do
     before(:each) do
       @group.update!(public: true)
     end
@@ -44,13 +42,12 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
 
     it '- When the member is admin, the outsider should see that the member is in the group.' do
-      Member::GroupAdminAction.new(@group_admin, @group, @group_members[1]).promote
-      expect(@group.group_members.detect { |m| m.member_id = @group_members[1].id }.is_master).to be true
+      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
       expect(Member::GroupList.new(@group_members[1], viewing_member: @outsider).member_of?(@group)).to be true
     end
   end
 
-  context '#member_of?: Is a member is an a public group in insider(who is in the group) point of view.' do
+  context '#member_of?: Is a member a public group in insider(who is in the group) point of view.' do
     before(:each) do
       @group.update!(public: true)
     end
@@ -60,12 +57,11 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
 
     it '- When the member is admin, the insider should see that the member is in the group.' do
-      Member::GroupAdminAction.new(@group_admin, @group, @group_members[1]).promote
-      expect(@group.group_members.detect { |m| m.member_id = @group_members[1].id }.is_master).to be true
+      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
       expect(Member::GroupList.new(@group_members[1], viewing_member: @group_members[3]).member_of?(@group)).to be true
     end
   end
 
-  
+
 
 end
