3965191db09e3551c1a52f1f5747df96753fb552
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-07-05 14:27:19 +0700

    renew specs for Member::GroupList.member_of?

--- spec/services/member/group_list_spec.rb
+++ spec/services/member/group_list_spec.rb
@@ -3,38 +3,37 @@ require 'rails_helper'
 pathname = Pathname.new(__FILE__)
 RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n Member::GroupList" do
 
-  before(:example) do
-    @group = FactoryGirl.create(:member_created_group, :with_members)
-    @group_admin = Group::MemberList.new(@group).admins[0]
-    @group_members = Group::MemberList.new(@group).members
-    @outsider = FactoryGirl.create(:member)
-  end
+  context '#member_of?: Is a member in a private group in someone point of view.' do
+    before(:context) do
+      @group = FactoryGirl.create(:member_created_group, :with_members)
+      @group_admin = Group::MemberList.new(@group).admins[0]
+      @group_members = Group::MemberList.new(@group).members
+      @outsider = FactoryGirl.create(:member)
+    end
 
-  context '#member_of?: Is a member in a group(not public) in insider(who is in the group) point of view.' do
     it '- When the member is ordinary member, the insider should see that the member is in the group.' do
       expect(Member::GroupList.new(@group_members[0], viewing_member: @group_members[2]).member_of?(@group)).to be true
     end
 
     it '- When the member is admin, the insider should see that the member is in the group.' do
-      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
-      expect(Member::GroupList.new(@group_members[1], viewing_member: @group_members[2]).member_of?(@group)).to be true
+      expect(Member::GroupList.new(@group_admin, viewing_member: @group_members[2]).member_of?(@group)).to be true
     end
-  end
 
-  context '#member_of?: Is a member in a group(not public) in outsider(who is not in the group) point of view.' do
     it '- When the member is ordinary member, the outsider should see that the member is in the group.' do
       expect(Member::GroupList.new(@group_members[0], viewing_member: @outsider).member_of?(@group)).to be false
     end
 
     it '- When the member is admin, the outsider should see that the member is in the group.' do
-      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
-      expect(Member::GroupList.new(@group_members[1], viewing_member: @outsider).member_of?(@group)).to be false
+      expect(Member::GroupList.new(@group_admin, viewing_member: @outsider).member_of?(@group)).to be false
     end
   end
 
-  context '#member_of?: Is a member a public group in outsider(who is not in the group) point of view.' do
-    before(:each) do
-      @group.update!(public: true)
+  context '#member_of?: Is a member in a public group in someone point of view.' do
+    before(:context) do
+      @group = FactoryGirl.create(:member_created_group, :with_members, public: true)
+      @group_admin = Group::MemberList.new(@group).admins[0]
+      @group_members = Group::MemberList.new(@group).members
+      @outsider = FactoryGirl.create(:member)
     end
 
     it '- When the member is ordinary member, the outsider should see that the member is in the group.' do
@@ -42,14 +41,7 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
 
     it '- When the member is admin, the outsider should see that the member is in the group.' do
-      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
-      expect(Member::GroupList.new(@group_members[1], viewing_member: @outsider).member_of?(@group)).to be true
-    end
-  end
-
-  context '#member_of?: Is a member a public group in insider(who is in the group) point of view.' do
-    before(:each) do
-      @group.update!(public: true)
+      expect(Member::GroupList.new(@group_admin, viewing_member: @outsider).member_of?(@group)).to be true
     end
 
     it '- When the member is ordinary member, the insider should see that the member is in the group.' do
@@ -57,9 +49,10 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
 
     it '- When the member is admin, the insider should see that the member is in the group.' do
-      @group.group_members.detect { |m| m.member_id == @group_members[1].id }.update!(is_master: true)
-      expect(Member::GroupList.new(@group_members[1], viewing_member: @group_members[2]).member_of?(@group)).to be true
+      expect(Member::GroupList.new(@group_admin, viewing_member: @group_members[2]).member_of?(@group)).to be true
     end
+
   end
+
 
 end
