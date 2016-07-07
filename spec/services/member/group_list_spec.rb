58b1748b55766b2dbab5431acfcf4e2bf68310b5
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-07-07 10:54:14 +0700

    add specs for Member::GroupList.as_admin

--- spec/services/member/group_list_spec.rb
+++ spec/services/member/group_list_spec.rb
@@ -52,6 +52,31 @@ RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\n
     end
   end
 
+  context '#as_admin: List groups which a member administrates, in someone point of view.' do
+    before(:all) do
+      @public_groups = FactoryGirl.create_list(:group, 2, public: true)
+      @private_groups = FactoryGirl.create_list(:group, 2)
+      @member_1 = FactoryGirl.create(:member_who_joins_groups, groups: [@public_groups[0], @private_groups[1]], is_admin: true)
+      @member_2 = FactoryGirl.create(:member_who_joins_groups, groups: [@private_groups[1]])
+      @member_3 = FactoryGirl.create(:member_who_joins_groups, groups: @public_groups)
+      @member_4 = FactoryGirl.create(:member)
+    end
 
+    it '- When the member view themself, the member should see all the groups they administrate.' do
+      expect(Member::GroupList.new(@member_1).as_admin).to match_array [@public_groups[0], @private_groups[1]]
+    end
+
+    it '- When the other member who share the same private groups is viewing, they should see only private groups they join, and public groups the member joins.' do
+      expect(Member::GroupList.new(@member_1, viewing_member: @member_2).as_admin).to \
+        match_array [@public_groups[0], @private_groups[1]]
+      expect(Member::GroupList.new(@member_1, viewing_member: @member_3).as_admin).to \
+        match_array [@public_groups[0]]
+    end
+
+    it "- When the other member who doesn't share any common private group is viewing, they shouldn't see any private group." do
+      expect(Member::GroupList.new(@member_1, viewing_member: @member_4).as_admin).to \
+        match_array [@public_groups[0]]
+    end
+  end
 
 end
