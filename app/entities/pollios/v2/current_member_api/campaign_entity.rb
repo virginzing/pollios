64e3fcdc49cd9b20b665d907b40a52096c3f7e88
zingfox <zingfox@icloud.com>
2016-07-15 15:51:47 +0700

    added expose :no_expiration in campaing entity

--- app/entities/pollios/v2/current_member_api/campaign_entity.rb
+++ app/entities/pollios/v2/current_member_api/campaign_entity.rb
@@ -1,12 +1,12 @@
 module Pollios::V2::CurrentMemberAPI
-	class CampaignEntity < Pollios::V2::BaseEntity
+  class CampaignEntity < Pollios::V2::BaseEntity
 
-		expose :id, as: :campaign_id
+    expose :id, as: :campaign_id
 
-		expose :name 
-		expose :description 
-		expose :get_photo_campaign, as: :photo_campaign, if: -> (obj, opts) { obj.get_photo_campaign.present? }
-		expose :get_original_photo_campaign, as: :original_photo_campaign, if: -> (obj, opts) { obj.get_original_photo_campaign.present? }
+    expose :name 
+    expose :description 
+    expose :get_photo_campaign, as: :photo_campaign, if: -> (_, _) { campaign.get_photo_campaign.present? }
+    expose :get_original_photo_campaign, as: :original_photo_campaign, if: -> (_, _) { campaign.get_original_photo_campaign.present? }
     expose :type_campaign, as: :type
 
     with_options(format_with: :as_integer) do
@@ -15,16 +15,25 @@ module Pollios::V2::CurrentMemberAPI
     end
 
     expose :owner
+    expose :no_expiration
 
-    expose :rewards do |obj, opts|
-      Pollios::V2::Shared::RewardEntity.represent obj.rewards, only: [:reward_id, :title, :detail, :total]
+    expose :rewards do |_, _|
+      Pollios::V2::Shared::RewardEntity.represent campaign.rewards, only: [:reward_id, :title, :detail, :total]
     end
     
-	private
+    private
 
-		def owner
+    def campaign
+      object
+    end
+
+    def owner
       entity = Pollios::V1::PollAPI::MemberInPollEntity
-			object.member.present? ? entity.represent(object.member) : entity.default_pollios_member
+      campaign.member.present? ? entity.represent(campaign.member) : entity.default_pollios_member
+    end
+
+    def no_expiration
+      campaign.unexpired?
     end
     
   end
