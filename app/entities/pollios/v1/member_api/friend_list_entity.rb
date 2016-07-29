57df20df6600ab68718aabf3dc244e4eaf1c27f3
zingfox <zingfox@icloud.com>
2016-07-29 16:11:06 +0700

    fixed ERROR unexpose follwers in api/member/friends AGAIN
    commit 29172a2a1939d0ed46570aa73aeba66aee7f99aa

--- app/entities/pollios/v1/member_api/friend_list_entity.rb
+++ app/entities/pollios/v1/member_api/friend_list_entity.rb
@@ -2,19 +2,26 @@ module Pollios::V1::MemberAPI
   class FriendListEntity < Pollios::V1::BaseEntity
 
     expose_members :friends
-    expose_members :followers, unless: -> (_, _) { object.member.citizen? }
+    expose_members :followers
     expose_members :followings
     expose_members :blocks
+
+    def followers
+      return [] if citizen_member?
+
+      object.followers
+    end
+
+    def blocks
+      return object.blocks if viewing_own_friends?
+
+      []
+    end
 
     private
 
     def current_member_linkage
       options[:current_member_linkage]
-    end
-
-    def blocks
-      return object.blocks if viewing_own_friends?
-      []
     end
 
     def current_member
@@ -23,6 +30,10 @@ module Pollios::V1::MemberAPI
 
     def viewing_own_friends?
       object.member.id == current_member.id
+    end
+
+    def citizen_member?
+      object.member.citizen?
     end
   end
 end
