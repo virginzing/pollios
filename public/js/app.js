eb10b810d43e0dcdf491a5fb7a5738161cc26954
lactozily <lactoz.ily@gmail.com>
2016-08-05 16:51:04 +0700

    finalize dashboard stats component design

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+// Sidebar Toggle
 (function() {
   var sidebarButton = document.getElementById('sidebar-button');
   var sidebar = document.getElementById('sidebar');
@@ -11,7 +12,7 @@
       openOverlay();
       overlay.removeEventListener('click', toggleSidebar);
     } else {
-      closeSidebar();      
+      closeSidebar();
       closeOverlay();
       overlay.addEventListener('click', toggleSidebar);
     }
@@ -27,7 +28,7 @@
   }
 
   function openSidebar() {
-    openElement(sidebar);    
+    openElement(sidebar);
   }
 
   function closeSidebar() {
@@ -47,7 +48,7 @@
         .split(' ')
         .filter(function(c) {
           return c !== 'open'
-        })   
+        })
         .join(' ');
   }
 
@@ -57,4 +58,69 @@
 
     elm.className = className.join(' ');
   }
-})()
+})();
+
+//ChartJS
+(function() {
+  var charts = document.querySelectorAll('div[pollios-chart]');
+  charts.forEach(createChart);
+
+  function createChart(chart) {
+    let data = createChartData(createDataset(extractChartAttr(chart)));
+    let canvas = createCanvas(chart);
+
+    Chart.Line(canvas, data);
+  }
+
+  function extractChartAttr(chart) {
+    return {
+      data: JSON.parse(chart.getAttribute('data')),
+      labels: JSON.parse(chart.getAttribute('data')),
+      color: chart.getAttribute('color')
+    }
+  }
+
+  function createCanvas(elm) {
+    let canvas = document.createElement('canvas');
+    elm.appendChild(canvas);
+    return canvas;
+  }
+
+  function createDataset(chartData) {
+    return {
+      labels: chartData.labels,
+      datasets: [
+          {
+              lineTension: 0.1,
+              fill: false,
+              borderWidth: 2,
+              borderColor: chartData.color,
+              pointRadius: 0,
+              data: chartData.data
+          }
+      ]
+    }
+  }
+
+  function createChartData(dataset) {
+    return {
+      data: dataset,
+      options: {
+        title: {
+          display: false
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [ {
+            display: false
+          } ],
+          yAxes: [ {
+            display: false
+          } ]
+        }
+      }
+    }
+  }
+})();
