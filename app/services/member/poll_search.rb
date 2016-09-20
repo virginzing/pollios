8a23019bcce18156138365d2d263a31747287a8a
zingfox <zingfox@icloud.com>
2016-09-20 19:04:37 +0700

    fixed member:poll_search popular_tag logic

--- app/services/member/poll_search.rb
+++ app/services/member/poll_search.rb
@@ -18,7 +18,7 @@ class Member::PollSearch
   end
 
   def popular
-    popular_tags
+    popular_tags.limit(10).map(&:name)
   end
 
   def polls_searched
@@ -58,15 +58,11 @@ class Member::PollSearch
   end
 
   def popular_tags
-    Tag.joins('LEFT OUTER JOIN taggings ON tags.id = taggings.tag_id')
-      .joins('LEFT OUTER JOIN polls ON taggings.poll_id = polls.id')
-      .where("polls.expire_status = 'f' AND polls.vote_all > 0 AND polls.in_group = 'f' AND polls.series = 'f'")
-      .select('tags.*, count(taggings.tag_id) as count')
-      .select('tags.*, max(taggings.created_at) AS tagged')
+    Tag.joins(:polls).merge(Poll.unscoped.viewing_by_member(member))
+      .select('tags.*, count(taggings.tag_id) as tag_count')
+      .select('tags.*, max(taggings.created_at) AS tagged_at')
       .group('tags.id')
-      .order('count desc, tagged desc')
-      .limit(10)
-      .map(&:name)
+      .order('tag_count desc, tagged_at desc')
   end
 
   def search_polls
