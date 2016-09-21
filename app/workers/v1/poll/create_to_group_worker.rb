6fd9164559c7981e37055d15326c293fef4812a6
zingfox <zingfox@icloud.com>
2016-09-21 15:19:35 +0700

    fixed error notification create poll to group

--- app/workers/v1/poll/create_to_group_worker.rb
+++ app/workers/v1/poll/create_to_group_worker.rb
@@ -5,7 +5,7 @@ class V1::Poll::CreateToGroupWorker
   def perform(member_id, poll_id, group_ids)
     member = Member.cached_find(member_id)
     poll = Poll.cached_find(poll_id)
-    group_list = Group.cached_find(group_ids)
+    group_list = Group.find(group_ids)
 
     Notification::Poll::CreateToGroup.new(member, poll, group_list)
   end
