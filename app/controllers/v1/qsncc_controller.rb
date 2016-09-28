4b16c5efa3da42928580650f53686d62d2df789c
lactozily <lactoz.ily@gmail.com>
2016-09-28 17:24:25 +0700

    refactor qsncc_controller to use before_action instead of error return

--- app/controllers/v1/qsncc_controller.rb
+++ app/controllers/v1/qsncc_controller.rb
@@ -4,24 +4,21 @@ module V1
 
     before_filter :set_group_qsncc
     before_action :set_meta, :validate_group
+    before_action :must_be_admin, only: [:close]
+    before_action :must_has_active_polls, only: [:get, :close]
+    before_action :must_has_closed_polls, only: [:result]
 
     def get
-      return redirect_to(action: 'result') if @group_qsncc.all_polls_already_close?
-
       @show_close_poll_button = v1_admin_signed_in?
       @poll = @group_qsncc.current_poll
     end
 
     def close
-      return redirect_to(action: 'get') unless v1_admin_signed_in?
-
       @group_qsncc.close_current_poll
       redirect_to(action: 'result')
     end
 
     def result
-      return redirect_to(action: 'get') if @group_qsncc.no_closed_poll?
-
       @show_next_poll_button = v1_admin_signed_in? && !@group_qsncc.all_polls_already_close?
       @poll = QsnccPollDecorator.new(@group_qsncc.recently_closed_poll)
     end
@@ -52,6 +49,14 @@ module V1
 
     def must_be_admin
       return redirect_to(action: 'get') unless v1_admin_signed_in?
+    end
+
+    def must_has_active_polls
+      return redirect_to(action: 'result') if @group_qsncc.all_polls_already_close?
+    end
+
+    def must_has_closed_polls
+      return redirect_to(action: 'get') if @group_qsncc.no_closed_poll?
     end
   end
 end
