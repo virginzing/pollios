5b513856bfc48ea4a7710e6838e6716b9d1a3fc4
ZingFox <zingfox@icloud.com>
2016-10-03 15:15:41 +0700

    fixed group_member_list_entity to use :member_invite_id

--- app/entities/pollios/v1/group_api/group_member_list_entity.rb
+++ app/entities/pollios/v1/group_api/group_member_list_entity.rb
@@ -2,12 +2,12 @@ module Pollios::V1::GroupAPI
   class GroupMemberListEntity < Pollios::V1::Shared::MemberEntity
     
     expose :invited_by do
-      expose :inviter_id, as: :member_id
+      expose :member_invite_id, as: :member_id
       expose :inviter_name, as: :name
     end
 
     def inviter_name
-      Member.cached_find(object.inviter_id).fullname
+      Member.cached_find(object.member_invite_id).fullname
     end
   end
 end
