09c04a1b618552131be0f209afbd20e1d58b41e7
CatFly Chittawan <c.catfly@gmail.com>
2016-10-04 01:31:21 +0700

    satisfied rubocop in GroupMember factories.

--- spec/factories/group_members.rb
+++ spec/factories/group_members.rb
@@ -20,25 +20,25 @@ FactoryGirl.define do
     is_master true
 
     trait :is_admin do
-    	is_master true
+      is_master true
     end
 
     trait :is_member do
-    	is_master false 
+      is_master false
     end
 
     trait :is_active do
-    	active true
+      active true
     end
 
     trait :is_pending do
-    	active false
+      active false
     end
 
     factory :group_member_that_is_admin, traits: [:is_admin]
     factory :group_member_that_is_member, traits: [:is_member]
-  	factory :group_member_that_is_active, traits: [:is_active]
-  	factory :group_member_that_is_pending, traits: [:is_pending]
+    factory :group_member_that_is_active, traits: [:is_active]
+    factory :group_member_that_is_pending, traits: [:is_pending]
   end
 
 end
