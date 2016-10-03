eee6810d745b431a241d955e1e70b90db94759bd
CatFly Chittawan <c.catfly@gmail.com>
2016-10-03 21:50:54 +0700

    added admin_exist validation to GroupMember model.

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -19,6 +19,12 @@ class GroupMember < ActiveRecord::Base
   belongs_to :member, touch: true
   belongs_to :group,  touch: true
 
+  validate :admin_exist
+
+  def admin_exist
+    errors.add(:base, "Group must have at least one admin") unless Group::MemberInquiry.new(self.group).admins?
+  end
+
   def self.have_request_group?(group, member)
     find_by(group: group, member: member, active: false).present?
   end
