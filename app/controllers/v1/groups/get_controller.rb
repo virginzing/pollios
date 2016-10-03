912cea7ea39c6c829529f01f554a559d801aef17
lactozily <lactoz.ily@gmail.com>
2016-10-03 18:11:49 +0700

    [WIP] refactor groups controller to use decorator instead

--- app/controllers/v1/groups/get_controller.rb
+++ app/controllers/v1/groups/get_controller.rb
@@ -4,16 +4,22 @@ module V1::Groups
 
     before_action :set_group
     before_action :set_meta
-    before_action :set_poll, only: [:poll_detail]
+    before_action :set_poll
+    before_action :must_be_admin, only: [:poll_detail_result]
 
     def detail
     end
 
     def poll_detail
+      @poll = Polls::DetailDecorator.new(@poll, params[:index])
+      @show_actions_button = v1_admin_signed_in?
+
       render('v1/groups/get/polls/detail')
     end
 
     def poll_detail_result
+      @poll = Polls::ResultDecorator.new(@poll, params[:index])
+
       render('v1/groups/get/polls/result')
     end
 
@@ -33,6 +39,10 @@ module V1::Groups
 
     def set_poll
       @poll = @group_qsncc.poll_by_index(params[:index])
+    end
+
+    def must_be_admin
+      return redirect_to(action: :poll_detail) unless v1_admin_signed_in?
     end
   end
 end
