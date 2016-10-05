c5cac12eba9a69edc097e414c0374fa568a62c77
CatFly Chittawan <c.catfly@gmail.com>
2016-10-05 19:20:26 +0700

    refactored push_data_only? in Notification::NewHelper into alert? and added some early return.

--- app/services/notification/push_helper.rb
+++ app/services/notification/push_helper.rb
@@ -10,29 +10,28 @@ module Notification::PushHelper
     recipient = device.member
     notification_data = notification_data(device.token, data)
 
-    return notification_data if push_data_only?(recipient, device, type)
+    return notification_data unless alert?(device, type)
 
     notification_data.merge(alert_data(recipient, message))
   end
 
-  def push_data_only?(recipient, device, type)
-    remove_action?(type) || turn_off_notification?(recipient, device, type)
+  def alert?(device, type)
+    return true if type == :always
+    return false if type == :never
+
+    receive_notification?(device, type)
   end
 
-  def remove_action?(type)
-    type == 'remove'
+  def receive_notification?(device, type)
+    recipient_receive_notification?(device.member, type) && device_receive_notification?(device)
   end
 
-  def turn_off_notification?(recipient, device, type)
-    recipient_turn_off_notification?(recipient, type) || device_turn_off_notification?(device)
+  def recipient_receive_notification?(recipient, type)
+    recipient.notification[type].to_b
   end
 
-  def recipient_turn_off_notification?(recipient, type)
-    !recipient.notification[type].to_b
-  end
-
-  def device_turn_off_notification?(device)
-    !device.receive_notification
+  def device_receive_notification?(device)
+    device.receive_notification
   end
 
   def notification_data(device_token, data)
