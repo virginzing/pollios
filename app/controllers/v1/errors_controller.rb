fda29e8c09a76587085716148270c807f90d12e2
lactozily <lactoz.ily@gmail.com>
2016-10-05 17:37:53 +0700

    fixed 404 error view missing template

--- app/controllers/v1/errors_controller.rb
+++ app/controllers/v1/errors_controller.rb
@@ -1,8 +1,19 @@
 module V1
   class ErrorsController < V1::ApplicationController
-    layout 'v1/navbar_no_sidebar'
+    layout 'v1/main'
+
+    before_action :set_meta
 
     def not_found
+    end
+
+    private
+
+    def set_meta
+      @meta ||= {
+        title: 'Not found',
+        description: 'The page you are looking for cannot be found'
+      }
     end
   end
 end
