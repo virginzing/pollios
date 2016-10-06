cf2491d89e1d527d9d5f891d33a45eeae48563cd
lactozily <lactoz.ily@gmail.com>
2016-10-06 16:41:28 +0700

    moved set_meta to v1/application_controller

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -1,12 +1,22 @@
 module V1
   class ApplicationController < ::ApplicationController
-    # rescue_from ArgumentError, with: :not_found_handler
     rescue_from ActionController::RoutingError, with: :not_found_handler
 
     private
 
     def not_found_handler
       render 'v1/errors/not_found', layout: 'v1/navbar_no_sidebar', status: :not_found
+    end
+
+    protected
+
+    def set_meta(meta)
+      @meta = meta
+
+      @meta[:title] ||= 'Pollios'
+      @meta[:description] ||= 'Raise your hand.'
+      @meta[:url] ||= 'http://www.pollios.com'
+      @meta[:image] ||= 'http://www.pollios.com/images/logo/1024logo.png'
     end
   end
 end
