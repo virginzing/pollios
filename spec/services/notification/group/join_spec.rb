5a13c4c352737949d2f8669733c2ab8f2062cdb7
CatFly Chittawan <c.catfly@gmail.com>
2016-10-07 17:32:58 +0700

    added matchers to Notification::Group::Join spec example.

--- spec/services/notification/group/join_spec.rb
+++ spec/services/notification/group/join_spec.rb
@@ -3,26 +3,40 @@ require 'rails_helper'
 pathname = Pathname.new(__FILE__)
 RSpec.describe "[Service: #{pathname.dirname.basename}/#{pathname.basename}]\n\nNotification::Group::Join" do
 
-  context 'Given a group with some members,' do
+  context 'Given a group with some members and a member not in group,' do
     before(:all) do
       @group = create(:group_with_members)
+      @some_member = create(:member)
     end
 
     specify 'everyone in the group can be notified if someone join the group.' do
       @group_members = Group::MemberList.new(@group).active
-      @some_member = create(:member)
 
       Notification::Group::Join.new(@some_member, @group)
 
-      @latest_notifications = NotifyLog \
-                              .where(recipient: @group_members) \
-                              .order(updated_at: :desc) \
-                              .group_by(&:recipient_id) \
-                              .map { |pair| pair[1][0] }
+      @latest_notification_logs = NotifyLog \
+                                  .where(recipient: @group_members) \
+                                  .order(updated_at: :desc) \
+                                  .group_by(&:recipient_id) \
+                                  .map { |pair| pair[1][0] }
 
-      expect(@latest_notifications.size).to be @group_members.size
-      expect(@latest_notifications.map(&:message)).to all include "#{@some_member.fullname} joined in #{@group.name} group."
-      expect(@latest_notifications.map(&:sender)).to all eq @some_member
+      expect(@latest_notification_logs.size).to be @group_members.size
+      expect(@latest_notification_logs.map(&:message)).to all include "#{@some_member.fullname} joined in #{@group.name} group."
+      expect(@latest_notification_logs.map(&:sender)).to all eq @some_member
+
+      @devices = @group_members.map(&:apn_devices).flatten
+      @device_tokens = @devices.map(&:token).map { |token| token.gsub(/\s+/, '') }
+
+      @latest_notification_pushes = Rpush::Apns::Notification
+                                    .where(device_token: @device_tokens)
+                                    .order(updated_at: :desc)
+                                    .group_by(&:device_token)
+                                    .map { |pair| pair[1][0] }
+
+      expect(@latest_notification_pushes.size).to be @devices.size
+
+      expect(@latest_notification_pushes.map { |push| push.data['group_id'] }).to all eq @group.id
+      expect(@latest_notification_pushes.map(&:alert)).to all include "#{@some_member.fullname} joined in #{@group.name} group."
     end
 
   end
