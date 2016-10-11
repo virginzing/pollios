77deb849fe8ca5005ee12b2f3b1c74998dde13e0
lactozily <lactoz.ily@gmail.com>
2016-10-11 11:51:43 +0700

    moved qrmode_image_url and open_app_url to polls/detail_for_guest_decorator.rb

--- app/controllers/v1/polls/get_controller.rb
+++ app/controllers/v1/polls/get_controller.rb
@@ -5,28 +5,18 @@ module V1::Polls
     rescue_from ExceptionHandler::UnprocessableEntity, with: :handle_status_422
 
     before_action :set_poll
-    before_action :set_poll_direct_access
     before_action :current_member_poll_action
-    before_action :current_member_poll_inquiry
 
-    before_action do
+    def detail
+      @poll = DetailForMemberDecorator.new(@poll, @current_member) if member_signed_in?
+      @poll = DetailForGuestDecorator.new(@poll)
+
       set_meta(
         title: @poll.title,
         description: @poll.member.fullname,
         url: request.original_url,
-        image: @poll_direct_access.qrcode_image_url
+        image: @poll.qrcode_image_url
       )
-    end
-
-    def detail
-      @poll_open_app_url = @poll_direct_access.open_app_url
-      @poll_qrcode_image_url = @poll_direct_access.qrcode_image_url
-
-      @already_voted = @current_member_poll_inquiry.voted? if member_signed_in?
-
-      return @poll = DetailForMemberDecorator.new(@poll, @current_member) if member_signed_in?
-
-      @poll = DetailForGuestDecorator.new(@poll)
     end
 
     private
@@ -45,12 +35,6 @@ module V1::Polls
 
     def current_member_poll_action
       @current_member_poll_action ||= ::Member::PollAction.new(@current_member, @poll)
-    end
-
-    def current_member_poll_inquiry
-      return false unless member_signed_in?
-
-      @current_member_poll_inquiry ||= ::Member::PollInquiry.new(@current_member, @poll)
     end
 
     def vote_params
