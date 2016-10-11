08d34497359a9709f6e05cd526bb43a9f5c6c82e
lactozily <lactoz.ily@gmail.com>
2016-10-11 12:06:30 +0700

    changed polls/post_controller to subclass from v1/application_controller

--- app/controllers/v1/polls/get_controller.rb
+++ app/controllers/v1/polls/get_controller.rb
@@ -4,8 +4,7 @@ module V1::Polls
 
     rescue_from ExceptionHandler::UnprocessableEntity, with: :handle_status_422
 
-    before_action :set_poll
-    before_action :current_member_poll_action
+    before_action :poll
 
     def detail
       @poll = DetailForMemberDecorator.new(@poll, @current_member) if member_signed_in?
@@ -25,20 +24,8 @@ module V1::Polls
       Base64.urlsafe_decode64(custom_key).to_i - ENV['POLL_URL_ENCODER_KEY'].to_i
     end
 
-    def set_poll
+    def poll
       @poll ||= ::Poll.find(decode_poll_id(params[:custom_key]))
-    end
-
-    def current_member_poll_action
-      @current_member_poll_action ||= ::Member::PollAction.new(@current_member, @poll)
-    end
-
-    def vote_params
-      params.permit(:choice_id)
-
-      params[:choice_id] = params[:choice_id].to_i unless params[:choice_id].nil?
-
-      params
     end
 
     def handle_status_422(e)
--- app/controllers/v1/polls/post_controller.rb
+++ app/controllers/v1/polls/post_controller.rb
@@ -1,9 +1,27 @@
 module V1::Polls
-  class PostController < GetController
+  class PostController < V1::ApplicationController
+
+    before_action :poll
+    before_action :current_member_poll_action
+
     def vote
       @current_member_poll_action.vote(vote_params)
 
       redirect_to :back
+    end
+
+    private
+
+    def current_member_poll_action
+      @current_member_poll_action ||= ::Member::PollAction.new(@current_member, @poll)
+    end
+
+    def vote_params
+      params.permit(:choice_id)
+
+      params[:choice_id] = params[:choice_id].to_i unless params[:choice_id].nil?
+
+      params
     end
   end
 end
