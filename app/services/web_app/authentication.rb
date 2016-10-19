8a217e1ebcd899a594c4b869a4f1779b401ff8b7
lactozily <lactoz.ily@gmail.com>
2016-10-19 13:48:30 +0700

    added sign_up in web_app authen service

--- app/services/web_app/authentication.rb
+++ app/services/web_app/authentication.rb
@@ -1,21 +1,33 @@
 class WebApp::Authentication
-  attr_reader :sign_in_failed
+  attr_reader :failed
 
   def sign_in(email, password)
     sign_in_params = { authen: email, password: password, app_name: 'pollios_web_app' }
     sentai_respond = ::Authentication::Sentai.sign_in(sign_in_params)
 
-    @sign_in_failed = sentai_respond['response_status'] != 'OK'
-    return nil if @sign_in_failed
+    @failed = sentai_respond['response_status'] != 'OK'
+    return nil if @failed
 
     authentication = ::Authentication.new(sentai_respond)
-    @sign_in_failed = !authentication.check_valid_member?
-    return nil if @sign_in_failed
+    @failed = !authentication.check_valid_member?
+    return nil if @failed
 
     authentication.member
   end
 
-  def sign_in_failed?
-    @sign_in_failed
+  def sign_up(email, password)
+    sign_up_params = { email: email, password: password, app_name: 'pollios_web_app' }
+    sentai_respond = ::Authentication::Sentai.sign_up(sign_up_params)
+
+    @failed = sentai_respond['response_status'] != 'OK'
+    return nil if @failed
+
+    authentication = ::Authentication.new(sentai_respond)
+
+    authentication.member
+  end
+
+  def failed?
+    @failed
   end
 end
