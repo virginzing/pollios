d16d5378a353e07d0d7d5831f45503f61b6fce2b
Rawitat Pulam <rawitat@code-app.com>
2017-02-19 11:31:37 +0700

    removed obsoleted mini-tests

--- test/models/user_to_group_service_test.rb
+++ test/models/user_to_group_service_test.rb
@@ -19,14 +19,4 @@ class UserToGroupServiceTest < ActiveSupport::TestCase
     assert_equal(@init.users.size, 1)
   end
 
-  def test_that_add_user_to_group
-    assert @init.add!
-  end
-
-  def test_that_create_group_member
-    assert_difference "GroupMember.count", 1 do
-      @init.add!
-    end
-  end
-
 end
